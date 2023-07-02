379ef908f9a51906a224742e239706c2b3b9e650
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-02 15:35:17 +0300

    Update UserMenuElement.tsx

--- app/components/navbar/UserMenuElement.tsx
+++ app/components/navbar/UserMenuElement.tsx
@@ -1,27 +1,24 @@
 'use client';
-
+import { COLORS } from '@/constants/colors';
+import styled from 'styled-components';
 interface MenuItemProps {
   onClick: () => void;
   label: string;
 }
 
+const TextDiv = styled.div`
+  margin: 4px 8px;
+  padding: 6px 8px;
+  color: ${COLORS.gray};
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  white-space: nowrap;
+  &:hover {
+    background-color: ${COLORS.textBgColor};
+  }
+`;
 const UserMenuElement = ({ onClick, label }: MenuItemProps) => {
-  return (
-    <div
-      onClick={onClick}
-      className='
-        px-4 
-        py-3 
-        hover:bg-neutral-100 
-        transition
-        font-semibold
-        whitespace-nowrap
-        
-      '
-    >
-      {label}
-    </div>
-  );
+  return <TextDiv onClick={onClick}>{label}</TextDiv>;
 };
 
 export default UserMenuElement;
