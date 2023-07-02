3473e8bca683a1ee1d7de852b34c42ee3d94844e
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-03 00:36:42 +0300

    Install react-responsive-carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import React from 'react';
-import Carrousel from './components/carrousel/Carrousel';
+import Carrousel from './components/carousel/ArtworkCarousel';
 
 export default function Home() {
   return (
     <div>
-      <Carrousel></Carrousel>
+      <Carrousel
+        images={[
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2_KsyjvB30UC5RVHpFmna6jr3P_-PvIxZ9w&usqp=CAU',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTisLZpbdmJ4kWDgUvpoWFG33PG2UQ0xgUZzw&usqp=CAU',
+        ]}
+        captions={['Test 1', 'Text 2']}
+      />
     </div>
   );
 }
