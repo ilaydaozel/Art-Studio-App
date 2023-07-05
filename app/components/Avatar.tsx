77306283b4e8118a2120a54949aafe39c08fc5c5
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-05 20:13:58 +0300

    Update Avatar.tsx

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -3,19 +3,23 @@
 import Image from 'next/image';
 
 interface AvatarProps {
-  src: string | null | undefined;
+  src?: string;
   height?: number;
   width?: number;
 }
 
-const Avatar = ({ src, height = 20, width = 20 }: AvatarProps) => {
+const Avatar = ({
+  src = 'https://cdn-icons-png.flaticon.com/512/666/666201.png',
+  height = 20,
+  width = 20,
+}: AvatarProps) => {
   return (
     <Image
       className='rounded-full'
       height={height}
       width={width}
       alt='Avatar'
-      src={src ? src : 'https://cdn-icons-png.flaticon.com/512/666/666201.png'}
+      src={src}
     />
   );
 };
