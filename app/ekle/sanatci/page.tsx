9bfdecb14bdc7942886e241476b377bdb7a633ef
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-07 00:50:33 +0300

    Add ArtistForm

--- app/ekle/sanatci/page.tsx
+++ app/ekle/sanatci/page.tsx
@@ -1,6 +1,13 @@
 'use client';
+
+import ArtistForm from '@/app/components/form/ArtistForm';
+
 const AddArtist = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ArtistForm></ArtistForm>
+    </div>
+  );
 };
 
 export default AddArtist;
