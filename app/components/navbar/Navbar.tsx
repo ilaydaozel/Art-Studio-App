c9578b1b1a6411fa2baef4c812e8eb2a9148700e
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-08 02:25:32 +0300

    Update Navbar.tsx

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,14 +1,13 @@
 'use client';
 import React from 'react';
-import Container from '../Container';
 import Logo from './Logo';
 import MenuElement from './MenuElement';
-import LogoTitle from './LogoTitle';
 import RegisterModal from '../modal/RegisterModal';
 import UserMenu from './UserMenu';
 import ArtworkContainer from '../artwork/ArtworkContainer';
 import styled from 'styled-components';
 import { User } from '@prisma/client';
+import { COLORS } from '@/constants/colors';
 interface NavbarProps {
   currentUser: User | null;
 }
@@ -20,57 +19,76 @@ const NavbarContainer = styled.div`
   z-index: 10;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
+const LogoTitle = styled.text`
+  font-weight: 700;
+  color: ${COLORS.darkGray};
+  font-size: 13px;
+`;
 const Navbar = ({ currentUser }: NavbarProps) => {
-  console.log({ currentUser });
   return (
-    <NavbarContainer>
-      <div
-        className='
-       py-3
+    <div className='pt-16 mt-1'>
+      <NavbarContainer>
+        <div
+          className='
+       py-4
        border-b-[1px]'
-      >
-        <Container>
+        >
           <div
-            className='
-          flex
-          flex-row
-          items-center
-          justify-between
-          gap-3
-          md:gap-0'
+            className='    
+            max-w-[2520px]
+            mx-auto 
+            xl:px-10
+            md:px-10
+            sm: px-2'
           >
             <div
               className='
           flex
           flex-row
+          flex-grow
+          items-center
+          justify-between
+          gap-6
+          md:gap-0'
+            >
+              <div
+                className='
+          flex
+          flex-row
           items-center
           justify-between
           gap-2'
-            >
-              <Logo height={30} width={30} />
-              <LogoTitle>KONAK KÜLTÜR SANAT AKADEMİSİ</LogoTitle>
-            </div>
-            <div
-              className='
+              >
+                <Logo height={30} width={30} />
+                <div className='flex flex-col'>
+                  <LogoTitle>KONAK KÜLTÜR</LogoTitle>
+                  <LogoTitle>SANAT AKADEMİSİ</LogoTitle>
+                </div>
+              </div>
+              <div
+                className='
               flex
               flex-row
               justify-between
               gap-12
              '
-            >
-              <MenuElement address='/' displayName='ANASAYFA' />
-              <MenuElement address='/sanatcilar' displayName='SANATÇILAR' />
-              <MenuElement address='/hakkinda' displayName='HAKKINDA' />
-              <MenuElement address='/sayfam' displayName='SAYFAM' />
-              <MenuElement address='/ekle/eser' displayName='ESER EKLE' />
-              <MenuElement address='/ekle/sanatci' displayName='SANATÇI EKLE' />
-            </div>
+              >
+                <MenuElement address='/' displayName='ANASAYFA' />
+                <MenuElement address='/sanatcilar' displayName='SANATÇILAR' />
+                <MenuElement address='/hakkinda' displayName='HAKKINDA' />
+                <MenuElement address='/ekle/eser' displayName='ESER EKLE' />
+                <MenuElement
+                  address='/ekle/sanatci'
+                  displayName='SANATÇI EKLE'
+                />
+              </div>
 
-            <UserMenu currentUser={currentUser} />
+              <UserMenu currentUser={currentUser} />
+            </div>
           </div>
-        </Container>
-      </div>
-    </NavbarContainer>
+        </div>
+      </NavbarContainer>
+    </div>
   );
 };
 
