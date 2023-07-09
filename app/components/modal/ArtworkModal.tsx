957db4693dfa2bf794ee8d3534423a6f0ad6add3
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-09 23:58:19 +0300

    Update ArtworkModal.tsx

--- app/components/modal/ArtworkModal.tsx
+++ app/components/modal/ArtworkModal.tsx
@@ -3,193 +3,45 @@
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-import dynamic from 'next/dynamic';
-import { useRouter } from 'next/navigation';
 import { useMemo, useState } from 'react';
 
-import useArtworkModal from '@/app/hooks/useArtworkModal';
-
 import Modal from './Modal';
-import ImageUpload from '../inputs/ImageUpload';
-import Input from '../inputs/Input';
-import Heading from '../Heading';
-
-enum STEPS {
-  INFORMATION = 0,
-  PHOTO = 1,
+import useArtworkModal from '@/app/hooks/useArtworkModal';
+import { UserArtwork } from '@prisma/client';
+import Image from 'next/image';
+interface ArtworkModalProps {
+  artwork?: UserArtwork | null;
 }
 
-const ArtworkModal = () => {
-  const router = useRouter();
+const DisplayArtworkModal = ({ artwork }: ArtworkModalProps) => {
+  const [isLoading, setIsLoading] = useState(false);
   const artworkModal = useArtworkModal();
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [step, setStep] = useState(STEPS.INFORMATION);
-
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    watch,
-    formState: { errors },
-    reset,
-  } = useForm<FieldValues>({
-    defaultValues: {
-      title: '',
-      description: '',
-      creationYear: '',
-      medium: '',
-      type: '',
-      width: 0,
-      height: 0,
-    },
-  });
-  const title = watch('title');
-  const description = watch('description');
-  const creationYear = watch('creationYear');
-  const type = watch('type');
-  const width = watch('width');
-  const height = watch('height');
-  const media = watch('media');
-
-  const setCustomValue = (id: string, value: any) => {
-    setValue(id, value, {
-      shouldDirty: true,
-      shouldTouch: true,
-      shouldValidate: true,
-    });
-  };
-
-  const onBack = () => {
-    setStep((value) => value - 1);
-  };
-
-  const onNext = () => {
-    setStep((value) => value + 1);
-  };
-
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    if (step !== STEPS.PHOTO) {
-      return onNext();
-    }
-
-    setIsLoading(true);
-
-    axios
-      .post('/api/', data)
-      .then(() => {
-        toast.success('Eser eklendi!');
-        window.location.reload();
-        reset();
-      })
-      .catch(() => {
-        toast.error('Bir şeyler yanlış gitti');
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
-
-  const actionLabel = useMemo(() => {
-    if (step === STEPS.PHOTO) {
-      return 'Create';
-    }
-
-    return 'Next';
-  }, [step]);
-
-  const secondaryActionLabel = useMemo(() => {
-    if (step === STEPS.INFORMATION) {
-      return undefined;
-    }
-
-    return 'Back';
-  }, [step]);
-
-  let bodyContent = (
-    <div className='flex flex-row justify-between flex-wrap'>
-      <Input
-        id='title'
-        label='Başlık'
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-        required
-      />
-      <Input
-        id='description'
-        label='Açıklama'
-        width='49%'
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-      />
-      <Input
-        id='creationYear'
-        label='Yapım Yılı'
-        width='49%'
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-      />
-      <Input
-        id='medium'
-        label='Teknik'
-        width='49%'
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-      />
-      <Input
-        id='type'
-        label='Tür'
-        width='49%'
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-      />
-      <Input
-        id='width'
-        label='Yükseklik'
-        width='49%'
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-      />
-      <Input
-        id='height'
-        label='Uzunluk'
-        width='49%'
-        disabled={isLoading}
-        register={register}
-        errors={errors}
+  const onSubmit = () => {};
+  const bodyContent = (
+    <div>
+      <Image
+        width={0}
+        height={0}
+        sizes='100vw'
+        style={{ width: '100%', height: 'auto' }}
+        src={artwork?.artworkMedias[0] || ''}
+        alt={'profile Image'}
       />
     </div>
   );
-
-  if (step === STEPS.PHOTO) {
-    bodyContent = (
-      <ImageUpload
-        label='Eser Fotoğrafı'
-        onChange={(value) => setCustomValue('media', value)}
-        value={media}
-      />
-    );
-  }
 
   return (
     <Modal
       disabled={isLoading}
       isOpen={artworkModal.isOpen}
-      title='Eser Ekle!'
-      actionLabel={actionLabel}
-      onSubmit={handleSubmit(onSubmit)}
-      secondaryActionLabel={secondaryActionLabel}
-      secondaryAction={step === STEPS.INFORMATION ? undefined : onBack}
+      title='Eser'
+      actionLabel='Kaydol'
       onClose={artworkModal.onClose}
+      onSubmit={onSubmit}
       body={bodyContent}
     />
   );
 };
 
-export default ArtworkModal;
+export default DisplayArtworkModal;
