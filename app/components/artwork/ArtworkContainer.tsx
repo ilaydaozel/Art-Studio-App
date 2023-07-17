52e8623bc8b0361f2f2dfa1c32f4737d6cb9fd97
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-18 00:19:00 +0300

    Update ArtworkContainer.tsx

--- app/components/artwork/ArtworkContainer.tsx
+++ app/components/artwork/ArtworkContainer.tsx
@@ -7,30 +7,25 @@ import Image from 'next/image';
 
 interface ArtworkContainerProps {
   artwork?: UserArtwork | null;
-  label: string;
 }
 
 const Container = styled.div`
   padding: 4px;
   width: 300px;
-  height: 60vh;
   background-color: #fff;
   margin: 30px 10px;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
 `;
-const ArtworkContainer = ({ artwork, label }: ArtworkContainerProps) => {
+const ArtworkContainer = ({ artwork }: ArtworkContainerProps) => {
   return (
     <Container>
-      <div className='w-[40vw]'>
-        <Image
-          width={0}
-          height={0}
-          sizes='100vw'
-          style={{ width: '100%', height: 'auto' }}
-          src={artwork?.artworkMedias[0] || ''}
-          alt={'profile Image'}
-        />
-      </div>
+      <Image
+        width={300}
+        height={0}
+        src={artwork?.artworkMedias[0] || ''}
+        alt={'artwork image'}
+      />
+      <h1>Başlık: {artwork?.title || ''}</h1>
     </Container>
   );
 };
