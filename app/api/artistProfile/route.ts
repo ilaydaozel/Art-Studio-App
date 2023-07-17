79a066c2fbba56cd21b0add23f9420cb67f57ff8
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-17 17:44:03 +0300

    Update artistProfile update operation

--- app/api/artistProfile/route.ts
+++ app/api/artistProfile/route.ts
@@ -1,38 +1,53 @@
 import { NextResponse } from "next/server";
-
 import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { User } from "@prisma/client";
 
-export async function POST(
-    request: Request,
-) {
+export async function POST(request: Request) {
     const currentUser = await getCurrentUser();
 
     if (!currentUser) {
         return NextResponse.error();
     }
-    //check if it is artist
-    if (!currentUser) {
+
+    const body = await request.json();
+    const { biography, profilePic, artworks } = body;
+    const user: User = currentUser.currentUser;
+
+    let artistProfile;
+
+    if (user.userType === "artist") {
+        artistProfile = await prisma.artistProfile.findFirst({
+            where: {
+                artistId: user.id,
+            },
+        });
+
+        if (artistProfile) {
+            artistProfile = await prisma.artistProfile.update({
+                where: {
+                    id: artistProfile.id,
+                },
+                data: {
+                    biography,
+                    profilePic,
+                    artworks,
+                },
+            });
+        } else {
+            artistProfile = await prisma.artistProfile.create({
+                data: {
+                    biography,
+                    profilePic,
+                    artworks,
+                    artistId: user.id,
+                },
+            });
+        }
+    } else {
         return NextResponse.error();
     }
 
-    const body = await request.json();
-    const {
-        biography,
-        profilePic,
-        artworks,
-    } = body;
-    const user: User = currentUser.currentUser;
-
-    const artistProfile = await prisma.artistProfile.create({
-        data: {
-            biography,
-            profilePic,
-            artworks,
-            artistId: user.id,
-        }
-    });
-
     return NextResponse.json(artistProfile);
 }
+
