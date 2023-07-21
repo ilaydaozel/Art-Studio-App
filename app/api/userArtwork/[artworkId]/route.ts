2e09de3f97eee0702ebd091542ef0d3ef5a574c8
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-21 03:50:14 +0300

    Create [artworkId] route

---
+++ app/api/userArtwork/[artworkId]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+    artworkId?: string;
+}
+
+
+export async function DELETE(request: Request, { params }: { params: IParams }) {
+    const { artworkId } = params;
+    // Check if the UserArtwork record exists
+    const existingArtwork = await prisma.userArtwork.findUnique({
+        where: {
+            id: artworkId,
+        },
+    });
+
+    if (!existingArtwork) {
+        return NextResponse.error();
+    }
+
+    // Delete the UserArtwork record
+    const deleteUserArtwork = await prisma.userArtwork.delete({
+        where: {
+            id: artworkId,
+        },
+    });
+
+    return NextResponse.json(deleteUserArtwork);
+}
