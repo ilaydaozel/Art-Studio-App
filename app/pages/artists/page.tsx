90b3a012c676660f97e39c8d2480045a3b268c19
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-24 17:18:21 +0300

    Show all the artists names

--- app/pages/artists/page.tsx
+++ app/pages/artists/page.tsx
@@ -1,5 +1,34 @@
-const Artists = () => {
-  return <div className='pt-20'>Sanatçılar</div>;
+import EmptyState from '@/app/components/EmptyState';
+import ClientOnly from '@/app/components/ClientOnly';
+import getAllArtistProfiles from '@/app/actions/getAllArtistProfiles';
+import { ArtistProfile } from '@prisma/client';
+import ArtistPreview from './ArtistPreview';
+
+const ArtistsPage = async () => {
+  const artists = await getAllArtistProfiles();
+  const artistsArray = artists?.artistProfile;
+  console.log('artists : ', artists);
+  console.log('artistsArray: ', artistsArray);
+  if (artistsArray?.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title='No artists found'
+          subtitle='Looks like you have no artists.'
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <div className='pt-20'>
+        {artistsArray?.map((artist) => (
+          <ArtistPreview artist={artist}></ArtistPreview>
+        ))}
+      </div>
+    </ClientOnly>
+  );
 };
 
-export default Artists;
+export default ArtistsPage;
