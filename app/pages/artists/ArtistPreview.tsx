629b4d50527e48d41ba50a3f9e0b42ac2aaf8c96
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-24 18:20:50 +0300

    Update ArtistPreview.tsx

--- app/pages/artists/ArtistPreview.tsx
+++ app/pages/artists/ArtistPreview.tsx
@@ -1,11 +1,37 @@
 'use client';
 
+import { COLORS } from '@/constants/colors';
+import styled from 'styled-components';
+import { ROUTE_PATHS } from '@/constants/routes';
+import { useRouter } from 'next/navigation';
+
 interface ArtistPreviewProps {
   artist: any;
 }
 
+const NameText = styled.button`
+  margin: 1%;
+  pointer: cursor;
+  color: ${COLORS.gray};
+  transition: color 0.2s;
+
+  &:hover {
+    color: ${COLORS.darkGray};
+  }
+`;
+
 const ArtistPreview = ({ artist }: ArtistPreviewProps) => {
-  return <div>{artist.user.name}</div>;
+  const router = useRouter();
+  const handleArtistClick = (artistId: string) => {
+    router.push(`${ROUTE_PATHS.ARTIST_PROFILE}/${artistId}`);
+  };
+  return (
+    <div onClick={() => handleArtistClick(artist.user.id)}>
+      <NameText>
+        {artist.user.name} {artist.user.surname}
+      </NameText>
+    </div>
+  );
 };
 
 export default ArtistPreview;
