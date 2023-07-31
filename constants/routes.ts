7a93b2347fb038aff97af17b8584009a25900224
ilaydaozel <ilaydaoozel@gmail.com>
2023-07-31 20:10:07 +0300

    Add edit artists to the routes

--- constants/routes.ts
+++ constants/routes.ts
@@ -4,7 +4,8 @@ export enum ROUTE_PATHS {
     HOME = '/',
     ARTISTS = '/pages/artists',
     ADD_ARTIST = '/pages/add/artist',
-    MY_PROFILE = '/pages/myProfile',
+    EDIT_ARTIST = '/pages/editArtists',
+    MY_PROFILE = '/pages/editProfile',
     ARTIST_PROFILE = '/pages/artistProfile',
     ABOUT = '/pages/about',
     UNAUTHORIZED = '/unauthorized',
@@ -13,9 +14,10 @@ export enum ROUTE_PATHS {
 export const ROUTE_NAMES: ChangeTypes<typeof ROUTE_PATHS, string> = {
     HOME: 'ANASAYFA',
     ARTISTS: 'SANATÇILAR',
-    ADD_ARTIST: 'SANATÇI EKLE',
+    ADD_ARTIST: 'YENİ SANATÇI EKLE',
     MY_PROFILE: 'PROFİLİMİ DÜZENLE',
     ARTIST_PROFILE: 'SANATÇI',
+    EDIT_ARTIST: 'SANATÇILARI DÜZENLE',
     ABOUT: 'HAKKIMIZDA',
     UNAUTHORIZED: 'Unauthorized',
 };
