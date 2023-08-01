a740731aa001a189e252da0dfca11622b71a87f8
ilaydaozel <ilaydaoozel@gmail.com>
2023-08-01 15:36:47 +0300

    Add transition to the footer when you scroll to the bottom

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,27 +1,26 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { User } from '@prisma/client';
 import { COLORS } from '@/constants/colors';
+import { ROUTE_PATHS, ROUTE_NAMES } from '@/constants/routes';
 
-const FooterContainer = styled.div`
-  width: 100%;
+const FooterContainer = styled.div<{ isVisible: boolean; translateY: number }>`
   position: fixed;
-  bottom: 0;
   left: 0;
+  bottom: ${(props) => (props.isVisible ? '0' : `-${props.translateY}px`)};
+  width: 100%;
   background-color: #ffffff;
   padding: 1% 0 1% 0;
   box-shadow: 0px -6px 6px -6px rgba(0, 0, 0, 0.1);
+  transition: bottom 0.4s ease-in-out;
 `;
 
-const MenuElement = styled.a`
+const FooterElement = styled.a`
   text-align: center;
-  padding: 2px 4px;
   color: ${COLORS.darkGray};
   cursor: pointer;
-  transition: text-decoration 0.3s;
   font-weight: 500;
-  font-size: 12px;
+  font-size: 10px;
   transition: font-weight 0.2s;
   &:hover {
     font-weight: 800;
@@ -36,18 +35,52 @@ const FooterText = styled.p`
 
 const LogoTitle = styled.a`
   color: ${COLORS.darkGray};
-  font-size: 1rem;
+  font-size: 0.8rem;
   font-weight: 600;
-  letter-spacing: 2px;
+  letter-spacing: 1.5px;
   cursor: pointer;
   display: flex;
   justify-content: left;
   flex: 2 1 auto;
-  margin: 0 0 0 24px;
 `;
+
 const Footer = () => {
+  const [isVisible, setIsVisible] = useState(
+    document.documentElement.scrollHeight <= window.innerHeight
+  );
+  const [footerHeight, setFooterHeight] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isOnBottom =
+        document.documentElement.clientHeight + window.scrollY >=
+        (document.documentElement.scrollHeight ||
+          document.documentElement.clientHeight);
+      console.log('isOnBottom ', isOnBottom);
+      setIsVisible(isOnBottom);
+    };
+
+    const handleResize = () => {
+      setFooterHeight(footerRef.current?.scrollHeight || 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initialize footer height on mount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [footerHeight]);
+
+  const footerRef = React.useRef<HTMLDivElement>(null);
+
   return (
-    <FooterContainer>
+    <FooterContainer
+      isVisible={isVisible}
+      translateY={footerHeight}
+      ref={footerRef}
+    >
       <div
         className='    
             max-w-[2520px]
@@ -56,7 +89,21 @@ const Footer = () => {
             md:px-10
             sm: px-2'
       >
-        <LogoTitle>KONAK KÜLTÜR SANAT AKADEMİSİ</LogoTitle>
+        <div className='flex flex-col items-start '>
+          <LogoTitle>KONAK KÜLTÜR SANAT AKADEMİSİ</LogoTitle>
+          <div className='flex flex-col gap-1 items-start my-2'>
+            <FooterElement href={ROUTE_PATHS.HOME}>
+              {ROUTE_NAMES.HOME}
+            </FooterElement>
+            <FooterElement href={ROUTE_PATHS.ARTISTS}>
+              {ROUTE_NAMES.ARTISTS}
+            </FooterElement>
+            <FooterElement href={ROUTE_PATHS.ABOUT}>
+              {ROUTE_NAMES.ABOUT}
+            </FooterElement>
+          </div>
+        </div>
+
         <FooterText>
           &copy; {new Date().getFullYear()} Konak Kültür Sanat Akademisi. Tüm
           hakları saklıdır.
