fee122f3b0db082cc485c03021bd81fc6457fbe0
ilaydaozel <ilaydaoozel@gmail.com>
2023-08-01 22:01:00 +0300

    Delete unnecessary imports

--- app/pages/artistProfile/[artistId]/ArtistProfileClient.tsx
+++ app/pages/artistProfile/[artistId]/ArtistProfileClient.tsx
@@ -4,18 +4,9 @@ import useBiographyModal from '@/app/hooks/useBiographyModal';
 import useProfilePictureModal from '@/app/hooks/useProfilePictureModal';
 import { COLORS } from '@/constants/colors';
 import styled from 'styled-components';
-import useAddArtworkModal from '@/app/hooks/useAddArtworkModal';
-import BiographyModal from '@/app/components/modal/BiographyModal';
 import { useRouter } from 'next/navigation';
-import ProfilePictureModal from '@/app/components/modal/ProfilePictureModal';
-import { FaRegEdit } from 'react-icons/fa';
-import { FaRegSquarePlus } from 'react-icons/fa6';
-import { MdDeleteForever } from 'react-icons/md';
-import { useState } from 'react';
 import ArtworkContainer from '@/app/components/artwork/ArtworkContainer';
 import { UserArtwork } from '@prisma/client';
-import { toast } from 'react-hot-toast';
-import axios from 'axios';
 
 const NameHeading = styled.text`
   font-size: 2rem;
@@ -55,10 +46,6 @@ interface ArtistProfileProps {
 }
 
 const ArtistProfile = ({ profileInfo, artworks }: ArtistProfileProps) => {
-  const router = useRouter();
-  const biographyModal = useBiographyModal();
-  const profilePictureModal = useProfilePictureModal();
-
   return (
     <div>
       <div className='p-[2vw]'>
