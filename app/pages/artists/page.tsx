e068099b0e39be6dfb78563e7bdfa1a3a4db0c65
ilaydaozel <ilaydaoozel@gmail.com>
2023-08-09 01:57:28 +0300

    Update page.tsx

--- app/pages/artists/page.tsx
+++ app/pages/artists/page.tsx
@@ -2,35 +2,63 @@ import EmptyState from '@/app/components/EmptyState';
 import ClientOnly from '@/app/components/ClientOnly';
 import getAllArtistProfiles from '@/app/actions/getAllArtistProfiles';
 import ArtistPreview from './ArtistPreview';
+import { IArtistProfile } from '@/app/actions/type';
+import { useRouter } from 'next/navigation';
+import { ROUTE_PATHS } from '@/constants/routes';
 
 const ArtistsPage = async () => {
-  const artists = await getAllArtistProfiles();
-  const artistsArray = artists?.artistProfile;
-  console.log('artists : ', artists);
-  console.log('artistsArray: ', artistsArray);
+  let artistProfiles: IArtistProfile[] = [];
 
-  if (artistsArray?.length === 0) {
+  try {
+    const result = await getAllArtistProfiles();
+    if (result && result.artistProfiles) {
+      artistProfiles = result.artistProfiles;
+      console.log('artistProfiles: ', artistProfiles);
+      if (artistProfiles.length > 0) {
+        return (
+          <ClientOnly>
+            <div className='pt-24  w-full'>
+              <div className='flex flex-wrap gap-4 px-12 my-8'>
+                {artistProfiles?.map((artist) => (
+                  <ArtistPreview
+                    key={artist.id}
+                    artist={artist}
+                  ></ArtistPreview>
+                ))}
+              </div>
+            </div>
+          </ClientOnly>
+        );
+      } else {
+        return (
+          <ClientOnly>
+            <EmptyState
+              title='No artists found'
+              subtitle='Looks like you have no artists.'
+            />
+          </ClientOnly>
+        );
+      }
+    } else {
+      return (
+        <ClientOnly>
+          <EmptyState
+            title='An error occured.'
+            subtitle='Looks like you have no artists.'
+          />
+        </ClientOnly>
+      );
+    }
+  } catch (error) {
     return (
       <ClientOnly>
         <EmptyState
-          title='No artists found'
+          title='An error occured.'
           subtitle='Looks like you have no artists.'
         />
       </ClientOnly>
     );
   }
-
-  return (
-    <ClientOnly>
-      <div className='pt-24  w-full'>
-        <div className='flex flex-wrap gap-4 px-12 my-8'>
-          {artistsArray?.map((artist) => (
-            <ArtistPreview key={artist.id} artist={artist}></ArtistPreview>
-          ))}
-        </div>
-      </div>
-    </ClientOnly>
-  );
 };
 
 export default ArtistsPage;
