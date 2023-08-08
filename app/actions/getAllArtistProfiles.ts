abe75b63fc1731e305361a37677fad8465d14fcf
ilaydaozel <ilaydaoozel@gmail.com>
2023-08-09 01:57:04 +0300

    Update getAllArtistProfiles.ts

--- app/actions/getAllArtistProfiles.ts
+++ app/actions/getAllArtistProfiles.ts
@@ -1,21 +1,20 @@
 import prisma from "@/app/libs/prismadb";
-import { ArtistProfile } from "@prisma/client";
+import { IArtistProfile } from "./type";
 
 
 export default async function getAllArtistProfiles() {
     try {
-        const artistProfile = await prisma.artistProfile.findMany({
+        const artistProfiles = await prisma.artistProfile.findMany({
             include: {
                 user: true
             }
         });
 
-        if (!artistProfile) {
+        if (!artistProfiles) {
             return null;
         }
-        console.log("artistProfile: ", artistProfile);
         return {
-            artistProfile: artistProfile as ArtistProfile[],
+            artistProfiles: artistProfiles as IArtistProfile[],
         };
     } catch (error: any) {
         throw new Error(error);
