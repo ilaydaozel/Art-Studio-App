e9f050b41dd8f24f262890e3693c76cb6c832c83
ilaydaozel <ilaydaoozel@gmail.com>
2023-08-19 01:59:55 +0300

    Update editArtistAccount

--- app/pages/edit/editArtistAccount/page.tsx
+++ app/pages/edit/editArtistAccount/page.tsx
@@ -2,29 +2,52 @@ import EmptyState from '@/app/components/EmptyState';
 import ClientOnly from '@/app/components/ClientOnly';
 import getAllArtistProfiles from '@/app/actions/getAllArtistProfiles';
 import EditArtistsClient from './EditArtistsClient';
+import { IArtistProfile } from '@/app/actions/type';
 
 const EditArtistsPage = async () => {
-  const artists = await getAllArtistProfiles();
-  const artistsArray = artists?.artistProfile;
-
-  if (artistsArray?.length === 0) {
+  let artistProfiles: IArtistProfile[] | null = null;
+  try {
+    const result = await getAllArtistProfiles();
+    if (result && result.artistProfiles) {
+      artistProfiles = result.artistProfiles;
+      if (artistProfiles.length > 0) {
+        return (
+          <ClientOnly>
+            <div className='pt-24  w-full'>
+              <EditArtistsClient artists={artistProfiles}></EditArtistsClient>
+            </div>
+          </ClientOnly>
+        );
+      } else {
+        return (
+          <ClientOnly>
+            <EmptyState
+              title='No artists found'
+              subtitle='Looks like you have no artists.'
+            />
+          </ClientOnly>
+        );
+      }
+    } else {
+      return (
+        <ClientOnly>
+          <EmptyState
+            title='An error occured.'
+            subtitle='Looks like you have no artists.'
+          />
+        </ClientOnly>
+      );
+    }
+  } catch (error) {
     return (
       <ClientOnly>
         <EmptyState
-          title='No artists found'
+          title='An error occured.'
           subtitle='Looks like you have no artists.'
         />
       </ClientOnly>
     );
   }
-
-  return (
-    <ClientOnly>
-      <div className='pt-24  w-full'>
-        <EditArtistsClient artists={artistsArray}></EditArtistsClient>
-      </div>
-    </ClientOnly>
-  );
 };
 
 export default EditArtistsPage;
