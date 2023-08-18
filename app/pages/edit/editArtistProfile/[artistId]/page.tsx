988ad7981f40dd6d698295863b53ae0c05ae5955
ilaydaozel <ilaydaoozel@gmail.com>
2023-08-19 01:59:08 +0300

    Update editArtistProfile

--- app/pages/edit/editArtistProfile/[artistId]/page.tsx
+++ app/pages/edit/editArtistProfile/[artistId]/page.tsx
@@ -1,22 +1,24 @@
 import getArtistProfileById from '@/app/actions/getArtistProfileById';
 import ClientOnly from '@/app/components/ClientOnly';
-import ArtistPage from './ArtistPage';
+import EditArtistProfile from './EditArtistProfileClient';
 import EmptyState from '@/app/components/EmptyState';
 import getAllArtworksByArtistId from '@/app/actions/getAllArtworksByArtistId';
+import { IArtistProfile, IUserArtwork } from '@/app/actions/type';
 
 interface IParams {
   artistId?: string;
 }
 
 const ArtistProfilePage = async ({ params }: { params: IParams }) => {
-  let artistProfile = null;
-  let allArtworks = null;
+  let artistProfile: { artistProfile: IArtistProfile } | null = null;
+  let allArtworks: { allUserArtworks: IUserArtwork[] } | null = null;
+
   if (params != undefined && params != undefined) {
     artistProfile = await getArtistProfileById(params);
     allArtworks = await getAllArtworksByArtistId(params);
   }
 
-  if (!artistProfile) {
+  if (!artistProfile?.artistProfile) {
     return (
       <ClientOnly>
         <EmptyState />
@@ -26,12 +28,10 @@ const ArtistProfilePage = async ({ params }: { params: IParams }) => {
 
   return (
     <ClientOnly>
-      <div className='pt-20'>
-        <ArtistPage
-          profileInfo={artistProfile?.artistProfile}
-          artworks={allArtworks?.allUserArtworks}
-        />
-      </div>
+      <EditArtistProfile
+        profileInfo={artistProfile.artistProfile}
+        artworks={allArtworks?.allUserArtworks}
+      />
     </ClientOnly>
   );
 };
