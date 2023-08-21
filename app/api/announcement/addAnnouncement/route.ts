60018f98a062e8e05f0a3b76006e7fc3849c78ce
ilaydaozel <ilaydaoozel@gmail.com>
2023-08-22 00:34:28 +0300

    Create new announcement api

---
+++ app/api/announcement/addAnnouncement/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(request: Request) {
+
+
+    const body = await request.json();
+    const {
+        caption,
+        subcaption,
+        link,
+        coverImage,
+        isActive,
+    } = body;
+
+    Object.keys(body).forEach((value: any) => {
+        if (!body[value]) {
+            NextResponse.error();
+        }
+    });
+
+    const announcement = await prisma.announcement.create({
+        data: {
+            caption,
+            subcaption,
+            link,
+            coverImage,
+            isActive,
+        }
+    });
+
+    return NextResponse.json(announcement);
+}
