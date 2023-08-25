a90914469d7cb4fce538789df0d623b3423582f9
ilaydaozel <ilaydaoozel@gmail.com>
2023-08-26 01:10:02 +0300

    Create ArtistAccountsList.tsx

---
+++ app/pages/edit/editArtistAccount/ArtistAccountsList.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { IUser } from '@/app/actions/type';
+import HeadingWithUnderline from '@/app/components/HeadingWithUnderline';
+import SlidingButton from '@/app/components/buttons/SlidingButton';
+import { ROUTE_PATHS } from '@/constants/routes';
+import { useRouter } from 'next/navigation';
+import ArtistAccountPreview from './ArtistAccountPreview';
+
+interface ArtistAccountsListProps {
+  accounts: IUser[];
+}
+
+const ArtistAccountsList = ({ accounts }: ArtistAccountsListProps) => {
+  const router = useRouter();
+
+  return (
+    <div className='flex flex-col gap-4 items-center justify-center'>
+      <div className='flex w-[80%] justify-end'>
+        <SlidingButton
+          label='Yeni Sanatçı Ekle +'
+          onClick={() =>
+            router.push(`${ROUTE_PATHS.ADD}${ROUTE_PATHS.ADD_NEW_ARTIST}`)
+          }
+        ></SlidingButton>
+      </div>
+      <HeadingWithUnderline
+        size={'1.2rem'}
+        title={'Sanatçı Hesaplarını Düzenle'}
+      ></HeadingWithUnderline>
+
+      <div className='w-[90%] pt-4 pb-4 flex flex-row flex-wrap items-center justify-around'>
+        {accounts.map((account: IUser) => (
+          <ArtistAccountPreview artist={account}></ArtistAccountPreview>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default ArtistAccountsList;
