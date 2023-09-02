72a19bfd31e34a5ebe24b455ad9b6127db0dc5f3
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-03 02:07:23 +0300

    Refactor create painting

--- app/components/virtualExhibition/Painting.tsx
+++ app/components/virtualExhibition/Painting.tsx
@@ -1,16 +1,12 @@
 import * as THREE from 'three';
 
-export const createPainting = (
-  url: string,
-  width: number,
-  height: number,
-  position: THREE.Vector3
-) => {
+export const createPainting = (url: string, width: number, height: number) => {
   const paintingTexture = new THREE.TextureLoader().load(url);
   const painting = new THREE.Mesh(
-    new THREE.BoxGeometry(width, height, 0.1),
+    new THREE.BoxGeometry(width, height, 0.2),
     new THREE.MeshBasicMaterial({ map: paintingTexture })
   );
-  painting.position.set(position.x, position.y, position.z);
+  painting.castShadow = true;
+  painting.receiveShadow = true;
   return painting;
 };
