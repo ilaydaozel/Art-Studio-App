c04c9cd5c2c67d6476f11691924f3ec1519aeaa5
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-03 17:56:13 +0300

    Refactor createAndHangPaintings

--- app/components/virtualExhibition/Painting.tsx
+++ app/components/virtualExhibition/Painting.tsx
@@ -1,10 +1,13 @@
 import { IUserArtwork } from '@/app/actions/type';
 import * as THREE from 'three';
 
-export const createPainting = (url: string, width: number, height: number) => {
+export const createPainting = (
+  url: string,
+  dimensions: { width: number; height: number }
+) => {
   const paintingTexture = new THREE.TextureLoader().load(url);
   const painting = new THREE.Mesh(
-    new THREE.BoxGeometry(width, height, 0.2),
+    new THREE.BoxGeometry(dimensions.width, dimensions.height, 0.2),
     new THREE.MeshLambertMaterial({ map: paintingTexture })
   );
   painting.castShadow = true;
@@ -14,32 +17,36 @@ export const createPainting = (url: string, width: number, height: number) => {
 
 export const createAndHangPaintings = (
   artworks: IUserArtwork[],
-  floorDimensions: { width: number; height: number },
-  scene: THREE.Scene
+  floorDimensions: { width: number; height: number }
 ) => {
   let distanceBetween = 20;
   let hangingHeight = 20;
   let wallIndex = 0;
-  let currentWallLength = 0; // Keeps track of the current wall's length
+  let currentWallLength = 0;
   let positionX = 0;
   let positionZ = 0;
   const floorWidth = floorDimensions.width;
   const floorHeight = floorDimensions.height;
-
+  const paintings: THREE.Mesh[] = [];
   for (let i = 0; i < artworks.length; i++) {
     const artwork: IUserArtwork = artworks[i];
     const artworkWidth: number = artwork.width ? artwork.width / 5 : 10;
     const artworkHeight: number = artwork.height ? artwork.height / 5 : 10;
-    const painting = createPainting(
-      artwork.artworkMedias[0],
-      artworkWidth,
-      artworkHeight
-    );
+    const painting = createPainting(artwork.artworkMedias[0], {
+      width: artworkWidth,
+      height: artworkHeight,
+    });
 
-    // Check if there's enough space on the current wall, otherwise, switch walls
-    if (currentWallLength + artworkWidth + distanceBetween > floorWidth) {
-      wallIndex = (wallIndex + 1) % 4; // Switch to the next wall
-      currentWallLength = 0; // Reset the current wall's length
+    if (wallIndex === 0 || wallIndex === 2) {
+      if (currentWallLength + artworkWidth + distanceBetween > floorWidth) {
+        wallIndex = (wallIndex + 1) % 4;
+        currentWallLength = 0;
+      }
+    } else {
+      if (currentWallLength + artworkWidth + distanceBetween > floorHeight) {
+        wallIndex = (wallIndex + 1) % 4;
+        currentWallLength = 0;
+      }
     }
 
     switch (wallIndex) {
@@ -66,10 +73,9 @@ export const createAndHangPaintings = (
         painting.rotation.y = Math.PI / 2;
         break;
     }
-    console.log(artwork.title, ': ', positionX, hangingHeight, positionZ);
     painting.position.set(positionX, hangingHeight, positionZ);
-    scene.add(painting);
-
+    paintings.push(painting);
     currentWallLength += artworkWidth + distanceBetween; // Update the current wall's length
   }
+  return paintings;
 };
