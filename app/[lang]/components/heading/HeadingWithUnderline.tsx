2be504929b8ce840f35745e7a6fd573f8cc29ebc
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-06 19:25:58 +0300

    Update location and styling of HeadingWithUnderline

--- app/[lang]/components/heading/HeadingWithUnderline.tsx
+++ app/[lang]/components/heading/HeadingWithUnderline.tsx
@@ -9,35 +9,28 @@ interface HeadingWithUnderlineProps {
 }
 
 const Container = styled.div`
-  width: 100%;
+  width: 84%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 const SectionTitle = styled.div<{ size: string }>`
-  width: 80%;
+  width: 100%;
   font-size: ${(props) => props.size};
-  font-weight: 500;
   color: ${COLORS.darkGray};
   text-align: left;
   margin: 2rem 0 0.25rem 0;
-`;
-
-const StyledDivider = styled.div`
-  width: 84%;
-  border-top: 1px solid #e5e7eb;
-  height: 1px;
+  border-bottom: 1px solid #e5e7eb;
 `;
 
 const HeadingWithUnderline = ({
   title,
-  size = '1.5rem',
+  size = '1.2rem',
 }: HeadingWithUnderlineProps) => {
   return (
     <Container>
       <SectionTitle size={size}>{title}</SectionTitle>
-      <StyledDivider />
     </Container>
   );
 };
