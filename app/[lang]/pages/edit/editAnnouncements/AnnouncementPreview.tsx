f7f363a10ed3558e497d7335c58b278efff4e779
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-06 14:25:40 +0300

    Refactor AnnouncementPreview by changing its content with Slide component

--- app/[lang]/pages/edit/editAnnouncements/AnnouncementPreview.tsx
+++ app/[lang]/pages/edit/editAnnouncements/AnnouncementPreview.tsx
@@ -2,6 +2,7 @@
 import styled from 'styled-components';
 import { IAnnouncement } from '@/app/[lang]/actions/type';
 import AnnouncementMenu from './AnnouncementMenu';
+import Slide from '@/app/[lang]/components/carousel/Slide';
 
 interface AnnouncementPreviewProps {
   announcement: IAnnouncement;
@@ -14,51 +15,9 @@ const AnnouncementContainer = styled.div`
   margin: 16px 4px;
 `;
 
-const NameText = styled.div`
-  color: #fff;
-  transition: color 0.2s;
-  position: absolute;
-  bottom: 2px;
-`;
-
-const AnnouncementBox = styled.div<{ picture: string }>`
-  display: flex;
-  justify-content: center;
+const AnnouncementBox = styled.div`
   width: 22vw;
   height: 22vh;
-  background-image: url(${(props) => props.picture});
-  background-size: cover;
-  background-position: center;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  overflow: hidden;
-  position: relative;
-  font-size: 1rem;
-  transition: font-size 0.2s background-color:0.2s;
-
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.3);
-    transition: background-color 0.2s;
-  }
-
-  &:hover::before {
-    background-color: rgba(0, 0, 0, 0.5);
-  }
-
-  @media (max-width: 768px) {
-    font-size: 1.2 rem;
-
-  }
-
-  @media (max-width: 480px) {
-    font-size: 1rem;
-  }
 `;
 
 const AnnouncementPreviewClient = ({
@@ -66,10 +25,8 @@ const AnnouncementPreviewClient = ({
 }: AnnouncementPreviewProps) => {
   return (
     <AnnouncementContainer>
-      <AnnouncementBox
-        picture={announcement.coverImage ? announcement.coverImage : ''}
-      >
-        <NameText>{announcement.caption}</NameText>
+      <AnnouncementBox>
+        <Slide slide={announcement}></Slide>
       </AnnouncementBox>
       <AnnouncementMenu announcement={announcement}></AnnouncementMenu>
     </AnnouncementContainer>
