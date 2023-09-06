9f599edae03a7a48b13571b8fab78cf8c4022b2f
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-06 17:35:47 +0300

    Update AnnouncementCarousel.tsx

--- app/[lang]/components/carousel/AnnouncementCarousel.tsx
+++ app/[lang]/components/carousel/AnnouncementCarousel.tsx
@@ -12,11 +12,21 @@ interface AnnouncementCarouselProps {
   slides: IAnnouncement[];
 }
 
-const NavButton = styled.button<{ position: string }>`
+const NavButtonRight = styled.button`
   position: absolute;
   bottom: 1.5rem;
-  left: ${(props) => props.position === 'left' && '1.5rem'};
-  right: ${(props) => props.position === 'right' && '1.5rem'};
+  left: 2rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.4);
+  font-size: 1.6rem;
+`;
+
+const NavButtonLeft = styled.button`
+  position: absolute;
+  bottom: 1.5rem;
+  right: 2rem;
   background: none;
   border: none;
   cursor: pointer;
@@ -44,14 +54,12 @@ const AnnouncementCarousel = ({ slides }: AnnouncementCarouselProps) => {
   return (
     <div className='w-[100vw] h-[100vh]'>
       <Slide slide={slides[activeIndex]}>
-        <div className='w-full'>
-          <NavButton position='left' onClick={goToPrevSlide}>
-            <BsFillArrowLeftSquareFill />
-          </NavButton>
-          <NavButton position='right' onClick={goToNextSlide}>
-            <BsFillArrowRightSquareFill />
-          </NavButton>
-        </div>
+        <NavButtonRight onClick={goToPrevSlide}>
+          <BsFillArrowLeftSquareFill />
+        </NavButtonRight>
+        <NavButtonLeft onClick={goToNextSlide}>
+          <BsFillArrowRightSquareFill />
+        </NavButtonLeft>
       </Slide>
     </div>
   );
