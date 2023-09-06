ec41470909bbcc73438304de59885a4a86472bdb
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-06 23:41:36 +0300

    Update Biography.tsx

--- app/[lang]/components/artistProfile/Biography.tsx
+++ app/[lang]/components/artistProfile/Biography.tsx
@@ -8,6 +8,11 @@ import TextButton from '../buttons/TextButton';
 import BiographyModal from '../modal/BiographyModal';
 import { useRouter } from 'next/navigation';
 
+interface BiographyProps {
+  artistProfile: IArtistProfile;
+  isEditable?: boolean;
+}
+
 const BiographyContent = styled.div`
   font-size: 1rem;
   font-weight: 500;
@@ -15,25 +20,6 @@ const BiographyContent = styled.div`
   word-break: break-all;
   padding: 10px 0;
 `;
-
-const InformaionContainer = styled.div`
-  width: full;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ArtworksContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-interface BiographyProps {
-  artistProfile: IArtistProfile;
-  isEditable: boolean;
-}
 
 const Biography = ({ artistProfile, isEditable = false }: BiographyProps) => {
   const biographyModal = useBiographyModal();
