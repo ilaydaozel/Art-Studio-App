3b75afd2050508f4a545c1831f0c51244f845856
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-07 12:42:36 +0300

    Update EditArtistAccountsClient.tsx

--- app/[lang]/pages/edit/editArtistAccounts/EditArtistAccountsClient.tsx
+++ app/[lang]/pages/edit/editArtistAccounts/EditArtistAccountsClient.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { IUser } from '@/app/[lang]/actions/type';
-import HeadingWithUnderline from '@/app/[lang]/components/heading/HeadingWithUnderline';
 import SlidingButton from '@/app/[lang]/components/buttons/SlidingButton';
 import { ROUTE_PATHS } from '@/constants/routes';
 import { useRouter } from 'next/navigation';
 import ArtistAccountsList from '@/app/[lang]/components/lists/ArtistAccountsList';
+import ListWithHeading from '@/app/[lang]/components/pageLayout/ListWithHeading';
+import styled from 'styled-components';
+import ListWithButton from '@/app/[lang]/components/pageLayout/ListWithButton';
 
 interface EditArtistAccountsClientProps {
   accounts: IUser[];
@@ -16,18 +18,21 @@ const EditArtistAccountsClient = ({
   const router = useRouter();
 
   return (
-    <div className='flex flex-col gap-4 items-center justify-center'>
-      <div className='flex w-[80%] justify-end'>
-        <SlidingButton
-          label='Yeni Sanatçı Ekle +'
-          onClick={() =>
-            router.push(`${ROUTE_PATHS.ADD}${ROUTE_PATHS.ADD_NEW_ARTIST}`)
-          }
-        ></SlidingButton>
-      </div>
-      <HeadingWithUnderline title='Sanatçı Hesaplarını Düzenle'></HeadingWithUnderline>
-      <ArtistAccountsList isEditable accounts={accounts}></ArtistAccountsList>
-    </div>
+    <>
+      <ListWithButton
+        buttonText='Yeni Sanatçı Ekle +'
+        onClick={() =>
+          router.push(`${ROUTE_PATHS.ADD}${ROUTE_PATHS.ADD_NEW_ARTIST}`)
+        }
+      >
+        <ListWithHeading headingText='Sanatçı Hesaplarını Düzenle'>
+          <ArtistAccountsList
+            isEditable
+            accounts={accounts}
+          ></ArtistAccountsList>
+        </ListWithHeading>
+      </ListWithButton>
+    </>
   );
 };
 export default EditArtistAccountsClient;
