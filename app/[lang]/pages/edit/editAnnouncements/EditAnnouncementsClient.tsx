0ab20ba476ed5b9f90aa17c9355e2a1707d4a80c
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-07 12:42:30 +0300

    Update EditAnnouncementsClient.tsx

--- app/[lang]/pages/edit/editAnnouncements/EditAnnouncementsClient.tsx
+++ app/[lang]/pages/edit/editAnnouncements/EditAnnouncementsClient.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { IAnnouncement } from '@/app/[lang]/actions/type';
-import HeadingWithUnderline from '@/app/[lang]/components/heading/HeadingWithUnderline';
-import SlidingButton from '@/app/[lang]/components/buttons/SlidingButton';
 import AddAnnouncementModal from '@/app/[lang]/components/modal/AddAnnouncementModal';
 import useAddAnnouncementModal from '@/app/[lang]/hooks/useAddAnnouncementModal';
 import AnnouncementsList from '@/app/[lang]/components/lists/AnnouncementsList';
+import ListWithHeading from '@/app/[lang]/components/pageLayout/ListWithHeading';
+import ListWithButton from '@/app/[lang]/components/pageLayout/ListWithButton';
 
 interface EditAnnouncementsClientProps {
   announcements: IAnnouncement[];
@@ -15,20 +15,20 @@ const EditAnnouncementsClient = ({
 }: EditAnnouncementsClientProps) => {
   const addAnnouncementModal = useAddAnnouncementModal();
   return (
-    <div className='flex flex-col gap-4 items-center justify-center'>
-      <div className='flex w-[80%] justify-end'>
-        <AddAnnouncementModal />
-        <SlidingButton
-          label='Yeni Duyuru Ekle +'
-          onClick={() => addAnnouncementModal.onOpen()}
-        ></SlidingButton>
-      </div>
-      <HeadingWithUnderline title='Duyuruları Düzenle'></HeadingWithUnderline>
-      <AnnouncementsList
-        isEditable
-        announcements={announcements}
-      ></AnnouncementsList>
-    </div>
+    <>
+      <AddAnnouncementModal />
+      <ListWithButton
+        buttonText='Yeni Duyuru Ekle +'
+        onClick={() => addAnnouncementModal.onOpen()}
+      >
+        <ListWithHeading headingText='Duyuruları Düzenle'>
+          <AnnouncementsList
+            isEditable
+            announcements={announcements}
+          ></AnnouncementsList>
+        </ListWithHeading>
+      </ListWithButton>
+    </>
   );
 };
 export default EditAnnouncementsClient;
