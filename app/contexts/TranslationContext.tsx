455539d2a3f6c6812a29f93e1664c11df28d622d
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-08 23:54:04 +0300

    Update TranslationContext.tsx

--- app/contexts/TranslationContext.tsx
+++ app/contexts/TranslationContext.tsx
@@ -1,71 +1,19 @@
-'use client';
-import { createContext, useContext } from 'react';
-import tr from '@/dictionaries/tr.json';
-const defaultLocalizationData = {
-  navbar: {
-    academy: {
-      name: '',
-    },
-    route_names: {
-      home: '',
-      artists: '',
-      add: '',
-      add_new_artist: '',
-      edit: '',
-      edit_artist_accounts: '',
-      edit_artist_profile: '',
-      edit_announcements: '',
-      artist_profile: '',
-      about: '',
-      virtual_exhibitions: '',
-      logout: '',
-    },
-    login_modal: {
-      login_successful_message: '',
-      login_failed_message: '',
-      password: '',
-      title: '',
-      subtitle: '',
-      action_label: '',
-    },
-  },
-  artistsPage: {
-    list_heading: '',
-  },
-  editArtistAccountsPage: {
-    list_heading: '',
-    add_button_text: '',
-  },
-  artistProfilePage: {
-    about: {
-      heading: '',
-    },
-    list: {
-      heading: '',
-    },
-  },
-  editArtistProfilePage: {
-    about: {
-      heading: '',
-    },
-    list: {
-      heading: '',
-      add_button_text: '',
-      max_artwork_number_warning: '',
-    },
-    header: {
-      change_image_button_text: '',
-      change_successful_message: '',
-      change_failed_message: '',
-      select_cover_image: '',
-      select_button_text: '',
-    },
-  },
-  editAnnouncementsPage: {
-    heading: '',
-    add_button_text: '',
-  },
-};
+import { createContext } from 'react';
+import { Action } from '../reducers/language/type';
+import { ILanguageData } from '@/languages/type';
+import EnglishData from '@/languages/en';
+import TurkishData from '@/languages/tr';
+import { Language } from '../types/language';
 
-export const TranslationContext = createContext(defaultLocalizationData);
-export const useTranslation = () => useContext(TranslationContext);
+export const TranslationContext = createContext<{
+  language: Language;
+  messages: ILanguageData;
+  switchLanguage: (newLang: Language) => Promise<void>;
+}>({
+  language: 'tr' as Language,
+  messages: TurkishData,
+  switchLanguage: async () => {},
+});
+export const TranslationDispatchContext = createContext<React.Dispatch<Action>>(
+  () => {}
+);
