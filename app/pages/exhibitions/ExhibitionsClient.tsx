f3e46666133f299e31e31292d543d24be43e76f4
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-13 23:57:33 +0300

    Create exhibitions page

---
+++ app/pages/exhibitions/ExhibitionsClient.tsx
@@ -0,0 +1,9 @@
+import { IExhibition } from '@/app/types';
+
+interface ExhibitionsClientProps {
+  exhibtions: IExhibition[];
+}
+const ExhibitionsClient = ({}: ExhibitionsClientProps) => {
+  return <></>;
+};
+export default ExhibitionsClient;
---
+++ app/pages/exhibitions/page.tsx
@@ -0,0 +1,53 @@
+import EmptyState from '@/app/components/EmptyState';
+import ClientOnly from '@/app/components/ClientOnly';
+import { IExhibition, IUser } from '@/app/types';
+import getAllExhibitions from '@/app/actions/getAllExhibitions';
+import ExhibitionsClient from './ExhibitionsClient';
+
+const ExhibitionsPage = async () => {
+  let exhibitons: IExhibition[] | null = null;
+  try {
+    const result = await getAllExhibitions();
+    if (result && result.exhibitons) {
+      exhibitons = result.exhibitons;
+      if (exhibitons.length > 0) {
+        return (
+          <ClientOnly>
+            <div className='pt-28 w-full'>
+              <ExhibitionsClient exhibtions={exhibitons}></ExhibitionsClient>
+            </div>
+          </ClientOnly>
+        );
+      } else {
+        return (
+          <ClientOnly>
+            <EmptyState
+              title='No exhibitons found'
+              subtitle='Looks like you have no exhibitons.'
+            />
+          </ClientOnly>
+        );
+      }
+    } else {
+      return (
+        <ClientOnly>
+          <EmptyState
+            title='An error occured.'
+            subtitle='Looks like you have no exhibitons.'
+          />
+        </ClientOnly>
+      );
+    }
+  } catch (error) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title='An error occured.'
+          subtitle='Looks like you have no exhibitons.'
+        />
+      </ClientOnly>
+    );
+  }
+};
+
+export default ExhibitionsPage;
