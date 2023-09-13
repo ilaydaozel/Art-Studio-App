34bba47840f617901c97688d54312f450f2d5e53
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-14 00:32:55 +0300

    Add exhibitions list to the page

--- app/pages/exhibitions/ExhibitionsClient.tsx
+++ app/pages/exhibitions/ExhibitionsClient.tsx
@@ -1,9 +1,15 @@
+import ComponentWithHeading from '@/app/components/layouts/ComponentWithHeading';
+import ExhibitionsList from '@/app/components/lists/ExhibitionsList';
 import { IExhibition } from '@/app/types';
 
 interface ExhibitionsClientProps {
-  exhibtions: IExhibition[];
+  exhibitions: IExhibition[];
 }
-const ExhibitionsClient = ({}: ExhibitionsClientProps) => {
-  return <></>;
+const ExhibitionsClient = ({ exhibitions }: ExhibitionsClientProps) => {
+  return (
+    <ComponentWithHeading headingText='hello'>
+      <ExhibitionsList exhibitions={exhibitions}></ExhibitionsList>
+    </ComponentWithHeading>
+  );
 };
 export default ExhibitionsClient;
