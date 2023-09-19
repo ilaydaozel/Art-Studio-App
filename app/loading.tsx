8b42da799f3f3fabf73cf6bb926f67670dfdad91
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-19 15:57:18 +0300

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,14 @@
+import { COLORS } from '@/constants/colors';
 import React from 'react';
 
 export default function Loading() {
   return (
-    <div className='min-h-screen max-w-screen flex items-center justify-center bg-slate-200'>
+    <div
+      style={{ backgroundColor: COLORS.lightGray }}
+      className='min-h-screen max-w-screen flex items-center justify-center'
+    >
       <div className='text-center'>
-        <div className='border-t-4 border-gray-200 border-solid rounded-full animate-spin h-12 w-12 mx-auto'></div>
+        <div className='border-t-4 border-gray-50 border-solid rounded-full animate-spin h-12 w-12 mx-auto'></div>
         <p className='text-3xl font-semibold mt-4 text-gray-800'>
           Konak Kültür Sanat Akademisi
         </p>
