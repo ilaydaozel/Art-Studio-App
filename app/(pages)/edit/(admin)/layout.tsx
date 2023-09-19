75094ff0a4bb2f4cac4ee8624f1e350f88564b2c
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-19 16:23:56 +0300

    Create layout for admin authorization

---
+++ app/(pages)/edit/(admin)/layout.tsx
@@ -0,0 +1,22 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import Unauthorized from '../Unauthorized';
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+  const user = currentUser?.currentUser;
+
+  if (!user) {
+    return <Unauthorized />;
+  } else {
+    const isAdmin: boolean = user?.userType === 'admin';
+    if (!isAdmin) {
+      return <Unauthorized isAdminAuthorization={true} />;
+    }
+  }
+
+  return <>{children}</>;
+}
