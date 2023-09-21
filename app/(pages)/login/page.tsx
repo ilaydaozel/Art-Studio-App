67df6ef16d53951dd4a3cdf04954554cebafef8b
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-21 19:59:33 +0300

    Create login page

---
+++ app/(pages)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from '@/app/components/form/Login';
+
+const LoginModal = () => {
+  return (
+    <div className='pt-28 pb-10 w-[90vw] m-auto md:w-[60vw]'>
+      <LoginForm />
+    </div>
+  );
+};
+export default LoginModal;
