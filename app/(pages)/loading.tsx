afe11588fd20f24cea02f475dee2b88427f5b7b9
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-21 17:17:16 +0300

    Update loading.tsx

--- app/(pages)/loading.tsx
+++ app/(pages)/loading.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 import Spinner from '../components/utils/Spinner';
 export default function Loading() {
   return (
-    <div className='min-h-screen max-w-screen flex items-center justify-center bg-black'>
+    <div
+      style={{ backgroundColor: COLORS.darkGray }}
+      className='min-h-screen max-w-screen flex items-center justify-center'
+    >
       <div className='text-center'>
         <Spinner />
         <p className='text-3xl font-semibold mt-4 text-slate-300'>
