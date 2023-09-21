4c069875cca8ef80c4431253026866111557337f
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-21 19:59:21 +0300

    Create parallel route for login

---
+++ app/(pages)/@modal/(.)login/page.tsx
@@ -0,0 +1,11 @@
+import Modal from '../../artistProfile/[artistId]/Modal';
+import LoginForm from '@/app/components/form/Login';
+
+const LoginModal = () => {
+  return (
+    <Modal>
+      <LoginForm />
+    </Modal>
+  );
+};
+export default LoginModal;
