71b77b8623a924c7abd41139d9cd9fb5a1420cfe
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-21 17:14:26 +0300

    Update loading.tsx

--- app/(pages)/loading.tsx
+++ app/(pages)/loading.tsx
@@ -1,15 +1,12 @@
 import { COLORS } from '@/constants/colors';
 import React from 'react';
-
+import Spinner from '../components/utils/Spinner';
 export default function Loading() {
   return (
-    <div
-      style={{ backgroundColor: COLORS.lightGray }}
-      className='min-h-screen max-w-screen flex items-center justify-center'
-    >
+    <div className='min-h-screen max-w-screen flex items-center justify-center bg-black'>
       <div className='text-center'>
-        <div className='border-t-4 border-gray-50 border-solid rounded-full animate-spin h-12 w-12 mx-auto'></div>
-        <p className='text-3xl font-semibold mt-4 text-gray-800'>
+        <Spinner />
+        <p className='text-3xl font-semibold mt-4 text-slate-300'>
           Konak Kültür Sanat Akademisi
         </p>
       </div>
