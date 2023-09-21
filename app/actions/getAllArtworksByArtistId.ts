99779be001fc57ccb225237286116844226fd77a
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-21 17:25:02 +0300

    Fix typo

--- app/actions/getAllArtworksByArtistId.ts
+++ app/actions/getAllArtworksByArtistId.ts
@@ -10,7 +10,7 @@ export default async function getAllArtworksByArtistId(
 ) {
     try {
         const { artistId } = params;
-        const allArtworks = await prisma.Artwork.findMany({
+        const allArtworks = await prisma.artwork.findMany({
             where: {
                 artistId: artistId,
             },
