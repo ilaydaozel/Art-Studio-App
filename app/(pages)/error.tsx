77edc718aa21a8c6df967126ed1470c8643b7468
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-26 02:18:09 +0300

    Update error.tsx

--- app/(pages)/error.tsx
+++ app/(pages)/error.tsx
@@ -9,10 +9,8 @@ export default function Error({ error, reset }: ErrorProps) {
   return (
     <div className='grid h-screen px-4 bg-white place-content-center'>
       <div className='text-center'>
-        <h1 className='font-black text-gray-200 text-9xl'>401</h1>
-
         <p className='text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-          Unauthroized!
+          Something went wrong ☹️
         </p>
 
         <p className='mt-4 text-gray-500'>
@@ -22,7 +20,7 @@ export default function Error({ error, reset }: ErrorProps) {
         <button
           type='button'
           onClick={() => reset()}
-          className='inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring cursor-pointer'
+          className='inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-black rounded hover:bg-gray-800 focus:outline-none focus:ring cursor-pointer'
         >
           Try Again
         </button>
