4e945566dfb4c8a7dd876e5412f17bd6eea7b604
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-26 23:33:21 +0300

    Update About.tsx

--- app/components/exhibition/About.tsx
+++ app/components/exhibition/About.tsx
@@ -4,35 +4,16 @@ import styled from 'styled-components';
 import useTranslate from '../../hooks/useTranslate';
 import { IExhibition } from '@/app/types';
 import { COLORS } from '@/constants/colors';
-import { getDateString } from '../utils/Helper';
+import ComponentWithHeading from '../layouts/ComponentWithHeading';
 
 interface AboutProps {
   exhibition: IExhibition;
   isEditable?: boolean;
 }
 
-const Container = styled.div`
-  display: flex;
-  gap: 4rem;
-  width: 90%;
-`;
-
-const TitleText = styled.div`
-  font-size: 2rem;
-`;
-
 const DescriptionText = styled.div`
-  font-size: 1.5rem;
+  font-size: 1rem;
   color: ${COLORS.darkGray};
-`;
-const DateText = styled.div`
-  font-size: 0.8rem;
-  color: ${COLORS.gray};
-`;
-const LeftWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 30%;
 `;
 
 const About = ({ exhibition, isEditable = false }: AboutProps) => {
@@ -43,17 +24,9 @@ const About = ({ exhibition, isEditable = false }: AboutProps) => {
   const t = useTranslate();
 
   return (
-    <>
-      <Container>
-        <LeftWrapper>
-          <TitleText>{exhibition.title}</TitleText>
-          <DateText>
-            {getDateString(exhibition.startDate, exhibition.endDate)}
-          </DateText>
-        </LeftWrapper>
-        <DescriptionText>{exhibition.description}</DescriptionText>
-      </Container>
-    </>
+    <ComponentWithHeading headingText={'Hakkında'}>
+      <DescriptionText>{exhibition.description}</DescriptionText>
+    </ComponentWithHeading>
   );
 };
 
