e0096ffd11c3baf5bd6407e762987cb9c0557416
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-26 21:26:41 +0300

    Update ExhibitionPreview.tsx

--- app/components/exhibition/ExhibitionPreview.tsx
+++ app/components/exhibition/ExhibitionPreview.tsx
@@ -3,69 +3,73 @@ import styled from 'styled-components';
 import { IExhibition } from '@/app/types';
 import Link from 'next/link';
 import { ROUTE_PATHS } from '@/constants/routes';
+import { getDateString } from '../utils/Helper';
 
 interface ExhibitionProps {
   exhibition: IExhibition;
 }
 
-const ExhibitonBox = styled.div<{ backgroundImgUrl: string }>`
+const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  gap: 4px;
+  margin: 10px;
+  transition: transform 0.2s;
+  &:hover {
+    transform: translateY(-0.8rem);
+  }
+`;
+
+const TitleText = styled.div`
+  font-size: 1rem;
+  font-weight: bold;
+  @media (max-width: 768px) {
+    font-size: 1 rem;
+  }
+  @media (max-width: 480px) {
+    font-size: 0.8rem;
+  }
+`;
+
+const DateText = styled.div`
+  font-size: 0.8rem;
+  @media (max-width: 768px) {
+    font-size: 0.6 rem;
+  }
+  @media (max-width: 480px) {
+    font-size: 0.4rem;
+  }
+`;
+
+const ExhibitonImage = styled.div<{ backgroundImgUrl: string }>`
   width: 20vw;
   height: 20vw;
   background-image: url(${(props) => props.backgroundImgUrl});
   background-size: cover;
-  border: 1px solid #ccc;
-  border-radius: 5px;
   overflow: hidden;
-  margin: 10px;
-  position: relative;
-  font-size: 1.5rem;
-  transition: font-size 0.2s background-color:0.2s;
-
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.3);
-    transition: background-color 0.2s;
-  }
-
-  &:hover::before {
-    background-color: rgba(0, 0, 0, 0.4);
-  }
-
-  &:hover {
-    font-size: 1.7rem;
-  }
 
   @media (max-width: 768px) {
     width: 30vw;
     height: 30vw;
-    font-size: 1.2 rem;
-    &:hover {
-      font-size: 1.4rem;
-    }
   }
 
-  @media (max-width: 480px) {
+  @media (max-width: 576px) {
     width: 40vw;
     height: 40vw;
-    font-size: 1rem;
-    &:hover {
-      font-size: 1.2rem;
-    }
   }
 `;
 
-const Exhibition = ({ exhibition }: ExhibitionProps) => {
+const ExhibitionPreview = ({ exhibition }: ExhibitionProps) => {
   return (
     <Link href={`${ROUTE_PATHS.EXHIBITION}/${exhibition.id}`}>
-      <ExhibitonBox backgroundImgUrl={exhibition.coverImage || ''} />
+      <Wrapper>
+        <ExhibitonImage backgroundImgUrl={exhibition.coverImage || ''} />
+        <TitleText>{exhibition.title}</TitleText>
+        <DateText>
+          {getDateString(exhibition.startDate, exhibition.endDate)}
+        </DateText>
+      </Wrapper>
     </Link>
   );
 };
-export default Exhibition;
+export default ExhibitionPreview;
