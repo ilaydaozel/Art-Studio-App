345faf91c517bb8abc6d608c0c9f20ddf5540e39
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-27 00:34:04 +0300

    Create delete exhibition

---
+++ app/api/exhibition/deleteExhibition/[exhibitionId]/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/lib/prismadb";
+
+interface IParams {
+    exhibitionId?: string;
+}
+
+export async function DELETE(request: Request, { params }: { params: IParams }) {
+    const { exhibitionId } = params;
+    const existingExhibition = await prisma.exhibition.findUnique({
+        where: {
+            id: exhibitionId,
+        },
+    });
+
+    if (!existingExhibition) {
+        console.log("Exhibition not found!")
+        console.log(NextResponse.error());
+        return NextResponse.error();
+    }
+
+    // Delete the Artwork record
+    const deletedExhibition = await prisma.exhibition.delete({
+        where: {
+            id: exhibitionId,
+        },
+    });
+
+    return NextResponse.json(deletedExhibition);
+}
