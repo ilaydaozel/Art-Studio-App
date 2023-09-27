024b3d2300db00eea5ffbe845eb24c11aa88b729
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-27 21:22:39 +0300

    Add popup to exhibition proile to select artworks

--- app/components/exhibition/ExhibitionProfile.tsx
+++ app/components/exhibition/ExhibitionProfile.tsx
@@ -2,18 +2,18 @@
 
 import styled from 'styled-components';
 import useTranslate from '../../hooks/useTranslate';
-import { IExhibition } from '@/app/types';
+import { IArtwork, IExhibition } from '@/app/types';
 import { useState } from 'react';
-import Link from 'next/link';
-import { ROUTE_PATHS } from '@/constants/routes';
 import Header from './Header';
 import About from './About';
-import VirtualExhibitionPreview from './VirtualExhibitionPreview';
 import ArtworkList from '../lists/ArtworkList';
 import ComponentWithHeading from '../layouts/ComponentWithHeading';
+import Popup from '../popup/Popup';
+import SlidingButton from '../buttons/SlidingButton';
 
 interface ExhibitionProfileProps {
   exhibition: IExhibition;
+  artworks?: IArtwork[];
   isEditable?: boolean;
 }
 
@@ -25,23 +25,55 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const ArtworkThumbnail = styled.img`
+  width: 20vw;
+  height: 40vh;
+  object-fit: cover;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.02);
+  }
+`;
 const ExhibitionProfile = ({
   exhibition,
+  artworks = [],
   isEditable = false,
 }: ExhibitionProfileProps) => {
-  const [showExhibition, setShowExhibition] = useState(false);
-
-  const location = {
-    element: 'list',
-    superElement: 'artist_profile',
-  };
-
-  const t = useTranslate();
+  const [showArtworkSelection, setShowArtworkSelection] = useState(false);
 
   return (
     <Container>
       <Header exhibition={exhibition}></Header>
       <About exhibition={exhibition}></About>
+      <SlidingButton
+        onClick={() => setShowArtworkSelection(true)}
+        label='Sistemden Resim Seç'
+      ></SlidingButton>
+      {showArtworkSelection && (
+        <Popup
+          onClose={() => {
+            setShowArtworkSelection(false);
+          }}
+          width='100%'
+          body={
+            <div className='flex justify-between gap-2'>
+              {artworks ? (
+                artworks.map((artwork) => (
+                  <ArtworkThumbnail
+                    key={artwork.id}
+                    src={artwork.artworkMedias[0] || ''}
+                  />
+                ))
+              ) : (
+                <p>There are no artworks!</p>
+              )}
+            </div>
+          }
+          title={'Resim seç'}
+          actionLabel={'Tamamla'}
+          onSubmit={() => {}}
+        ></Popup>
+      )}
       <ComponentWithHeading headingText='Katılan Eserler'>
         <ArtworkList artworks={exhibition.artworks}></ArtworkList>
       </ComponentWithHeading>
