f9c823c261ae8be495b357d28ef6cb276509f70b
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-27 19:09:41 +0300

    Fix error

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -31,11 +31,7 @@ const EmptyState: React.FC<EmptyStateProps> = ({
       <Heading center title={title} subtitle={subtitle} />
       <div className='w-48 mt-4'>
         {showReset && (
-          <Button
-            outline
-            label='Remove all filters'
-            onClick={() => router.push('/')}
-          />
+          <Button label='Remove all filters' onClick={() => router.push('/')} />
         )}
       </div>
     </div>
