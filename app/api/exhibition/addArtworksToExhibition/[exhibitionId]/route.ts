5eb297fe9ecca72ad05bc3f4d5a7220033d927e9
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-28 23:10:51 +0300

    Create addArtworksToExhibition api

---
+++ app/api/exhibition/addArtworksToExhibition/[exhibitionId]/route.ts
@@ -0,0 +1,64 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/lib/prismadb";
+import { IArtwork } from "@/app/types";
+
+interface IParams {
+    exhibitionId: string;
+}
+
+
+export async function POST(request: Request, { params }: { params: IParams }) {
+    const { exhibitionId } = params;
+    const body = await request.json();
+    const { selectedArtworks } = body;
+
+    try {
+        const selectedArtworkIds = selectedArtworks.map((artwork: IArtwork) => artwork.id);
+
+        const exhibition = await prisma.exhibition.findFirst({
+            where: {
+                id: exhibitionId,
+            },
+        });
+
+        if (!exhibition) {
+            return NextResponse.json({ error: "Exhibition not found." });
+        }
+
+        const newArtworkIds = selectedArtworkIds.filter(
+            (artwork: IArtwork) => !exhibition.artworkIds.includes(artwork.id)
+        );
+
+        if (newArtworkIds.length === 0) {
+            return NextResponse.json({ error: "Selected artworks are already in the exhibition." });
+        }
+
+        const updatedExhibition = await prisma.exhibition.update({
+            where: {
+                id: exhibitionId,
+            },
+            data: {
+                artworkIds: { set: [...(exhibition.artworkIds), ...newArtworkIds], },
+            },
+        });
+
+        newArtworkIds.map(async (artworkId: string) => {
+            await prisma.artwork.update({
+                where: {
+                    id: artworkId,
+                },
+                data: {
+                    exhibitionIds: {
+                        push: exhibitionId,
+                    },
+                },
+            });
+        }
+        );
+
+        return NextResponse.json(updatedExhibition);
+    } catch (error) {
+        console.error("Error updating exhibition:", error);
+        return NextResponse.json({ error: "An error occurred while updating the exhibition." });
+    }
+}
