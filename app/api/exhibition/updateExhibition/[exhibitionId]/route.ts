702ac2fde36353c3333c174e96160dac43ae1a8b
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-28 16:32:18 +0300

    Update exhibition and artwork when new artwork is added to the exhibition

--- app/api/exhibition/updateExhibition/[exhibitionId]/route.ts
+++ app/api/exhibition/updateExhibition/[exhibitionId]/route.ts
@@ -1,42 +1,60 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prismadb";
+import { IArtwork } from "@/app/types";
 
 interface IParams {
-    exhibitionId?: string;
+    exhibitionId: string;
 }
 
 export async function POST(request: Request, { params }: { params: IParams }) {
-
     const { exhibitionId } = params;
     const body = await request.json();
     const { selectedArtworks } = body;
     console.log("selectedArtworks: ", selectedArtworks);
-    let exhibition;
 
-    exhibition = await prisma.exhibition.findFirst({
-        where: {
-            id: exhibitionId,
-        },
-    });
+    try {
+        const selectedArtworkIds = selectedArtworks.map((artwork: IArtwork) => artwork.id);
 
-    if (exhibition) {
-        exhibition = await prisma.exhibition.update({
+        const exhibition = await prisma.exhibition.findFirst({
+            where: {
+                id: exhibitionId,
+            },
+        });
+
+        if (!exhibition) {
+            return NextResponse.json({ error: "Exhibition not found." });
+        }
+        const existingArtworkIds = exhibition.artworkIds || [];
+        const updatedArtworkIds = [...existingArtworkIds, ...selectedArtworkIds];
+
+        const updatedExhibition = await prisma.exhibition.update({
             where: {
                 id: exhibitionId,
             },
             data: {
-                artworks: selectedArtworks
+                artworkIds: updatedArtworkIds,
+
             },
         });
-    } else {
-        return NextResponse.json({ error: "exhibition not found." });
+
+        selectedArtworks.map(async (artwork: IArtwork) => {
+            await prisma.artwork.update({
+                where: {
+                    id: artwork.id,
+                },
+                data: {
+                    exhibitionIds: {
+                        set: [...(artwork.exhibitionIds), exhibitionId],
+                    },
+                },
+            });
+        }
+        );
+
+
+        return NextResponse.json(updatedExhibition);
+    } catch (error) {
+        console.error("Error updating exhibition:", error);
+        return NextResponse.json({ error: "An error occurred while updating the exhibition." });
     }
-
-
-    return NextResponse.json(exhibition);
 }
-
-
-
-
-
