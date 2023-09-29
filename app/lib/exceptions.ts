27f7a9bfc748801137b085f9909142d29ea5f130
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-29 18:09:53 +0300

    Create authentication exceptions

--- app/lib/exceptions.ts
+++ app/lib/exceptions.ts
@@ -1,11 +1,18 @@
 export class AuthRequiredError extends Error {
-    constructor(message = "Auth is required to access this page") {
+    constructor(message = "Authentication is required to access this page!") {
         super(message);
         this.name = "AuthRequiredError";
     }
 }
+
+export class AdminAuthRequiredError extends Error {
+    constructor(message = "Admin authentication is required to access this page!") {
+        super(message);
+        this.name = "AdminAuthRequiredError";
+    }
+}
 export class PasswordLengthError extends Error {
-    constructor(message = "Şifre en az 4 karakter uzunluğunda olmalıdır.") {
+    constructor(message = "Şifre en az 4 karakter uzunluğunda olmalıdır!") {
         super(message);
         this.name = "PasswordLengthError";
     }
