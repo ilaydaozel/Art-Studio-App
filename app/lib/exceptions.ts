a26c7560bb964dc2ecc24b7dfd95e8dd040bb746
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-29 16:25:57 +0300

    Add new exceptions

--- app/lib/exceptions.ts
+++ app/lib/exceptions.ts
@@ -4,3 +4,23 @@ export class AuthRequiredError extends Error {
         this.name = "AuthRequiredError";
     }
 }
+export class PasswordLengthError extends Error {
+    constructor(message = "Şifre en az 4 karakter uzunluğunda olmalıdır.") {
+        super(message);
+        this.name = "PasswordLengthError";
+    }
+}
+
+export class PasswordMismatchError extends Error {
+    constructor(message = "Şifreler aynı değil!") {
+        super(message);
+        this.name = "PasswordMismatchError";
+    }
+}
+
+export class UnknownError extends Error {
+    constructor(message = "Bir sorun oluştu!") {
+        super(message);
+        this.name = "UnknownError";
+    }
+}
