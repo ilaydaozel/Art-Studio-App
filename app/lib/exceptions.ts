1ce9f1eb70f56c6ed164b7b3686d2e0da74a0ce1
ilaydaozel <ilaydaoozel@gmail.com>
2023-09-29 20:50:33 +0300

    Update exceptions.ts

--- app/lib/exceptions.ts
+++ app/lib/exceptions.ts
@@ -1,33 +1,28 @@
 export class AuthRequiredError extends Error {
-    constructor(message = "Authentication is required to access this page!") {
+    constructor(message = "authRequiredError") {
         super(message);
-        this.name = "AuthRequiredError";
     }
 }
 
 export class AdminAuthRequiredError extends Error {
-    constructor(message = "Admin authentication is required to access this page!") {
+    constructor(message = "adminAuthRequiredError") {
         super(message);
-        this.name = "AdminAuthRequiredError";
     }
 }
 export class PasswordLengthError extends Error {
-    constructor(message = "Şifre en az 4 karakter uzunluğunda olmalıdır!") {
+    constructor(message = "passwordLengthError") {
         super(message);
-        this.name = "PasswordLengthError";
     }
 }
 
 export class PasswordMismatchError extends Error {
-    constructor(message = "Şifreler aynı değil!") {
+    constructor(message = "passwordMismatchError") {
         super(message);
-        this.name = "PasswordMismatchError";
     }
 }
 
 export class UnknownError extends Error {
-    constructor(message = "Bir sorun oluştu!") {
+    constructor(message = "unknownError") {
         super(message);
-        this.name = "UnknownError";
     }
 }
