cf3e296df8c5dd831721067986c7a1510530d481
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-01 15:43:15 +0300

    Create a window on the wall

--- app/components/virtualExhibition/Room.tsx
+++ app/components/virtualExhibition/Room.tsx
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { CSG } from 'three-csg-ts';
 
 const roomHeight = 40;
 
@@ -40,29 +41,59 @@ const createCeiling = (width: number, height: number) => {
 
 const createWall = (wallColor: string, width: number) => {
   const wall = new THREE.Mesh(
-    new THREE.BoxGeometry(width, roomHeight, 0.001),
+    new THREE.BoxGeometry(width, roomHeight, 0.1),
     new THREE.MeshPhongMaterial({ color: wallColor })
   );
   wall.position.y = roomHeight / 2;
   return wall;
 };
 
-const createAllWalls = (width: number, height: number) => {
+const createWallWithWindow = (floorHeight: number) => {
+  const rightWall = createWall('#F8F3FF', floorHeight);
+
+  // Create the window geometry and material
+  const windowGeometry = new THREE.BoxGeometry(30, 30, 1); // Adjust the size as needed
+  const windowMaterial = new THREE.MeshLambertMaterial({ color: 'brown' });
+  const window = new THREE.Mesh(windowGeometry, windowMaterial);
+
+  // Position the window within the right wall (adjust coordinates as needed)
+  window.position.set(20, 0, 0);
+
+  rightWall.updateMatrix();
+  window.updateMatrix();
+
+  // Convert meshes to CSG objects
+  const rightWallCSG = CSG.fromMesh(rightWall);
+  const windowCSG = CSG.fromMesh(window);
+  // Subtract the window from the right wall using CSG
+  const wallWithHoleCSG = rightWallCSG.subtract(windowCSG);
+
+  // Create a new mesh from the resulting CSG object
+  const wallWithHoleMesh = CSG.toMesh(
+    wallWithHoleCSG,
+    rightWall.matrix,
+    rightWall.material
+  );
+
+  return wallWithHoleMesh;
+};
+
+const createAllWalls = (floorWidth: number, floorHeight: number) => {
   const wallGroup = new THREE.Group();
   const wallColor: string = '#FFFFFF';
-  const frontWall = createWall(wallColor, width);
-  frontWall.position.z = -height / 2;
+  const frontWall = createWall(wallColor, floorWidth);
+  frontWall.position.z = -floorHeight / 2;
 
-  const leftWall = createWall(wallColor, height);
-  leftWall.position.x = -width / 2;
+  const leftWall = createWall(wallColor, floorHeight);
+  leftWall.position.x = -floorWidth / 2;
   leftWall.rotation.y = Math.PI / 2;
 
-  const rightWall = createWall(wallColor, height);
-  rightWall.position.x = width / 2;
+  const rightWall = createWallWithWindow(floorHeight);
+  rightWall.position.x = floorWidth / 2;
   rightWall.rotation.y = Math.PI / 2;
 
-  const backWall = createWall(wallColor, width);
-  backWall.position.z = height / 2;
+  const backWall = createWall(wallColor, floorWidth);
+  backWall.position.z = floorHeight / 2;
 
   wallGroup.add(leftWall, frontWall, rightWall, backWall);
   return wallGroup;
