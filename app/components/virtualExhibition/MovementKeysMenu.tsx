0827b5a2332e4ca0b58cd2dd080154365abac0a2
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-02 21:56:01 +0300

    Update MovementKeysMenu.tsx

--- app/components/virtualExhibition/MovementKeysMenu.tsx
+++ app/components/virtualExhibition/MovementKeysMenu.tsx
@@ -1,28 +1,29 @@
 import {
-  BsFillArrowLeftSquareFill,
-  BsFillArrowRightSquareFill,
-  BsFillArrowDownSquareFill,
-  BsFillArrowUpSquareFill,
-} from 'react-icons/bs';
+  HiArrowRight,
+  HiArrowLeft,
+  HiArrowUp,
+  HiArrowDown,
+} from 'react-icons/hi';
 
 export const MovementKeysMenu = () => {
   return (
     <div className='relative'>
-      <BsFillArrowUpSquareFill
-        id='up-icon'
-        className='absolute md:bottom-10 bottom-8 md:left-10 left-8 text-neutral-500 hover:text-neutral-700 opacity-50 md:w-8 w-6 md:h-8 h-6'
+      <HiArrowUp
+        id='iconUp'
+        className='absolute md:bottom-10 bottom-8 md:left-10 left-8 p-1.5 bg-neutral-500 hover:bg-neutral-700 opacity-50 text-neutral-100 rounded-md md:w-8 w-6 md:h-8 h-6'
       />
-      <BsFillArrowDownSquareFill
-        id='down-icon'
-        className='absolute bottom-0 md:left-10 left-8 text-neutral-500 hover:text-neutral-700 opacity-50 md:w-8 w-6 md:h-8 h-6'
+      <HiArrowDown
+        id='iconDown'
+        className='absolute bottom-0 left-10 p-1.5 bg-neutral-500 hover:bg-neutral-700 opacity-50 text-neutral-100 rounded-md md:w-8 w-6 md:h-8 h-6'
       />
-      <BsFillArrowRightSquareFill
-        id='right-icon'
-        className='absolute md:left-20 left-16 bottom-0 text-neutral-500 hover:text-neutral-700 opacity-50 md:w-8 w-6 md:h-8 h-6'
+      <HiArrowRight
+        id='iconRight'
+        className='absolute md:left-20 left-16 bottom-0 p-1.5 bg-neutral-500 hover:bg-neutral-700 opacity-50 text-neutral-100 rounded-md md:w-8 w-6 md:h-8 h-6'
       />
-      <BsFillArrowLeftSquareFill
-        id='left-icon'
-        className='absolute left-0 bottom-0 text-neutral-500 hover:text-neutral-700 opacity-50 md:w-8 w-6 md:h-8 h-6'
+
+      <HiArrowLeft
+        id='iconLeft'
+        className='absolute left-0 bottom-0 p-1.5 bg-neutral-500 hover:bg-neutral-700 opacity-50 text-neutral-100 rounded-md md:w-8 w-6 md:h-8 h-6'
       />
     </div>
   );
