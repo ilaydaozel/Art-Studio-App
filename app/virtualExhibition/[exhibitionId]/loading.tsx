9ba5fe6b9ab2086b84de04b4e491fa272f621abc
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-04 02:39:31 +0300

    Update loading.tsx

--- app/virtualExhibition/[exhibitionId]/loading.tsx
+++ app/virtualExhibition/[exhibitionId]/loading.tsx
@@ -1,14 +1,20 @@
+import Spinner from '@/app/components/utils/Spinner';
 import { COLORS } from '@/constants/colors';
 import React from 'react';
+
 export default function Loading() {
   return (
     <div
-      style={{ backgroundColor: COLORS.darkGray }}
+      style={{ backgroundColor: COLORS.lightGray }}
       className='min-h-screen max-w-screen flex items-center justify-center'
     >
       <div className='text-center'>
-        <p className='text-3xl font-semibold mt-4 text-slate-300'>
+        <Spinner />
+        <p className='text-3xl font-semibold mt-4 text-slate-700'>
           Sanal Sergi
+        </p>
+        <p className='text-3xl font-semibold mt-4 text-slate-700'>
+          Konak Kültür Sanat Akademisi
         </p>
       </div>
     </div>
