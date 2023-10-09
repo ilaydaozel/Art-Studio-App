3ccb13136235448fcb8d79822317806ac344e2b5
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-10 01:43:15 +0300

    Update virtual exhibition loading page

--- app/virtualExhibition/[exhibitionId]/loading.tsx
+++ app/virtualExhibition/[exhibitionId]/loading.tsx
@@ -5,15 +5,15 @@ import React from 'react';
 export default function Loading() {
   return (
     <div
-      style={{ backgroundColor: COLORS.lightGray }}
-      className='min-h-screen max-w-screen flex items-center justify-center'
+      style={{ backgroundColor: COLORS.darkGray }}
+      className='min-h-screen max-w-screen flex items-center justify-center z-20'
     >
       <div className='text-center'>
         <Spinner />
-        <p className='text-3xl font-semibold mt-4 text-slate-700'>
+        <p className='text-3xl font-semibold mt-4 text-slate-400'>
           Sanal Sergi
         </p>
-        <p className='text-3xl font-semibold mt-4 text-slate-700'>
+        <p className='text-3xl font-semibold mt-4 text-slate-300'>
           Konak Kültür Sanat Akademisi
         </p>
       </div>
