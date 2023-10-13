e5525d3e3ced634101e3d080a4591aec0171be9a
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-13 20:58:57 +0300

    Uppdate style by making the fontweight bold when the link is active

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { signOut } from 'next-auth/react';
 import styled from 'styled-components';
 import { COLORS } from '@/constants/colors';
@@ -18,55 +18,35 @@ const MenuContainer = styled.div`
   justify-content: flex-start;
   align-items: flex-start;
   gap: 10px;
-  font-size: 0.8rem;
 `;
-const UserMenuElement = styled.a`
-  color: ${COLORS.darkGray};
+
+const UserMenuElement = styled.a<{ isActive?: boolean }>`
+  color: ${COLORS.gray};
   cursor: pointer;
+  font-weight: ${(props) => props.isActive && 'bold'};
   transition: background-color 0.3s ease;
   white-space: nowrap;
   font-size: 0.8rem;
   &:hover {
     font-weight: 700;
   }
-
-  @media (max-width: 768px) {
-    font-size: 0.6rem;
-  }
-
-  @media (max-width: 480px) {
-    font-size: 0.5rem;
-  }
-`;
-
-const NameText = styled.div`
-  color: ${COLORS.darkGray};
-  width: 100%;
-  font-size: 0.9rem;
-  font-weight: bold;
-  border-bottom: 1px solid ${COLORS.lightGray};
-  @media (max-width: 768px) {
-    font-size: 0.7rem;
-  }
-  @media (max-width: 480px) {
-    font-size: 0.6rem;
-  }
 `;
 
 const UserMenu = ({ user }: UserMenuProps) => {
   const router = useRouter();
+  const pathname = usePathname();
 
   const location = { element: 'route_names' };
   const t = useTranslate();
 
   return (
     <MenuContainer>
-      <NameText>
-        {user.name} {user.surname}
-      </NameText>
       {user.userType === 'artist' ? (
         <>
           <UserMenuElement
+            isActive={
+              pathname === ROUTE_PATHS.EDIT + ROUTE_PATHS.EDIT_ARTIST_PROFILE
+            }
             href={`${ROUTE_PATHS.EDIT}${ROUTE_PATHS.EDIT_ARTIST_PROFILE}/${user.id}`}
           >
             {t('edit_artist_profile', location)}
@@ -75,21 +55,33 @@ const UserMenu = ({ user }: UserMenuProps) => {
       ) : (
         <>
           <UserMenuElement
+            isActive={
+              pathname === ROUTE_PATHS.EDIT + ROUTE_PATHS.EDIT_ARTIST_ACCOUNTS
+            }
             href={`${ROUTE_PATHS.EDIT}${ROUTE_PATHS.EDIT_ARTIST_ACCOUNTS}`}
           >
             {t('edit_artist_accounts', location)}
           </UserMenuElement>
           <UserMenuElement
+            isActive={
+              pathname === ROUTE_PATHS.EDIT + ROUTE_PATHS.EDIT_ANNOUNCEMENTS
+            }
             href={`${ROUTE_PATHS.EDIT}${ROUTE_PATHS.EDIT_ANNOUNCEMENTS}`}
           >
             {t('edit_announcements', location)}
           </UserMenuElement>
           <UserMenuElement
+            isActive={
+              pathname === ROUTE_PATHS.EDIT + ROUTE_PATHS.EDIT_EXHIBITIONS
+            }
             href={`${ROUTE_PATHS.EDIT}${ROUTE_PATHS.EDIT_EXHIBITIONS}`}
           >
             {t('edit_exhibitions', location)}
           </UserMenuElement>
-          <UserMenuElement href={`${ROUTE_PATHS.REGISTER}`}>
+          <UserMenuElement
+            isActive={pathname === ROUTE_PATHS.EDIT + ROUTE_PATHS.REGISTER}
+            href={`${ROUTE_PATHS.REGISTER}`}
+          >
             {t('register', location)}
           </UserMenuElement>
         </>
