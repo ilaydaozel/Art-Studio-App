0425d2e0756ef683e913b63422c9dfddce2e9c78
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-14 00:13:05 +0300

    Refactor api call with new helper function

--- app/components/modal/UpdateTextModal.tsx
+++ app/components/modal/UpdateTextModal.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react';
-import { toast } from 'react-hot-toast';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useBiographyModal from '@/app/hooks/useUpdateTextModal';
 import Modal from './Modal';
 import { useRouter } from 'next/navigation';
-import { AxiosResponse } from 'axios';
 import useTranslate from '@/app/hooks/useTranslate';
+import { updateData } from '../utils/Helper';
 
 interface BiographyModalProps {
   script: string;
@@ -24,12 +23,7 @@ const BiographyModal = ({
   const biographyModal = useBiographyModal();
   const router = useRouter();
   const t = useTranslate();
-  const exceptionsLocation = { element: 'exceptions' };
   const location = { element: 'update_text_modal' };
-
-  const refreshPage = () => {
-    router.refresh();
-  };
 
   const {
     register,
@@ -45,26 +39,14 @@ const BiographyModal = ({
   const text = watch('text');
 
   const submit: SubmitHandler<FieldValues> = async () => {
-    setIsLoading(true);
-    try {
-      onSubmit(text).then((response: AxiosResponse<any, any>) => {
-        if (response.data?.error) {
-          toast.error(t(response.data.error, exceptionsLocation));
-        } else {
-          toast.success(t('update_successful_message', location));
-          biographyModal.onClose();
-          refreshPage();
-        }
-      });
-    } catch (error) {
-      toast.error(
-        error instanceof Error
-          ? t(error.message, exceptionsLocation)
-          : t('unknownError', exceptionsLocation)
-      );
-    } finally {
-      setIsLoading(false);
-    }
+    await updateData(
+      onSubmit(text),
+      setIsLoading,
+      t,
+      location,
+      biographyModal.onClose,
+      router
+    );
   };
 
   const bodyContent = (
