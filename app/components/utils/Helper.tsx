3e22ae69232fde87f45c3c81b138d6b298776a3b
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-14 00:12:15 +0300

    Create a helper function to make api calls safely

--- app/components/utils/Helper.tsx
+++ app/components/utils/Helper.tsx
@@ -1,9 +1,14 @@
+import { toast } from 'react-hot-toast';
+import { AxiosResponse } from 'axios';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+
 function formatMonth(month: number) {
   if (month < 10) {
     return '0' + month;
   }
   return month;
 }
+
 export const getDateString = (
   startDateInput: string,
   endDateInput: string
@@ -28,4 +33,36 @@ export const getDateString = (
   }
 
   return startDateString + ' - ' + endDateString;
+};
+
+export const updateData = async (
+  submitResponse: Promise<AxiosResponse<any, any>>,
+  setIsLoading: (isLoading: boolean) => void,
+  t: (key: string, location: any) => string,
+  location: any,
+  onClose: () => void,
+  router: AppRouterInstance
+) => {
+  const exceptionsLocation = { element: 'exceptions' };
+  setIsLoading(true);
+  console.log(submitResponse);
+  try {
+    submitResponse.then((response: AxiosResponse<any, any>) => {
+      if (response.data?.error) {
+        toast.error(t(response.data.error, exceptionsLocation));
+      } else {
+        toast.success(t('update_successful_message', location));
+        onClose();
+        router.refresh();
+      }
+    });
+  } catch (error) {
+    toast.error(
+      error instanceof Error
+        ? t(error.message, exceptionsLocation)
+        : t('unknownError', exceptionsLocation)
+    );
+  } finally {
+    setIsLoading(false);
+  }
 };
