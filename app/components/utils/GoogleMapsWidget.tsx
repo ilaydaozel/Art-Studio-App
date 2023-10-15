bf4d4d3293293def5b5f65950d756fa278ca846c
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-15 22:39:31 +0300

    Lazyload Google maps with the IntersectionObserver API

--- app/components/utils/GoogleMapsWidget.tsx
+++ app/components/utils/GoogleMapsWidget.tsx
@@ -1,5 +1,6 @@
-import React, { useMemo } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { GoogleMap, Marker } from '@react-google-maps/api';
+import Spinner from './Spinner';
 
 interface GoogleMapsWidgetProps {
   mapContainerStyle?: { width: string; height: string };
@@ -7,22 +8,60 @@ interface GoogleMapsWidgetProps {
 const GoogleMapsWidget = ({
   mapContainerStyle = { width: '50%', height: '400px' },
 }: GoogleMapsWidgetProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const containerRef = useRef(null);
   const center = useMemo(
     () => ({ lat: 38.41948699951172, lng: 27.132444381713867 }),
     []
   );
   const apiKey = useMemo(() => process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY, []);
 
+  const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+      setIsLoading(true);
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+      script.async = true;
+      script.onload = () => {
+        setIsLoading(false);
+      };
+      document.head.appendChild(script);
+    }
+  };
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0,
+    });
+    observer.observe(container);
+    return () => {
+      observer.unobserve(container);
+    };
+  }, []);
+
   return (
-    <LoadScript googleMapsApiKey={apiKey || ''}>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        center={center}
-        zoom={14}
-      >
-        <Marker position={center} title='Konak Kültür Sanat Akademisi' />
-      </GoogleMap>
-    </LoadScript>
+    <div
+      ref={containerRef}
+      style={mapContainerStyle}
+      className='outline outline-1 outline-neutral-400 flex justify-center items-center'
+    >
+      {!isLoading ? (
+        <GoogleMap
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          center={center}
+          zoom={14}
+        >
+          <Marker position={center} title='Konak Kültür Sanat Akademisi' />
+        </GoogleMap>
+      ) : (
+        <Spinner />
+      )}
+    </div>
   );
 };
 
