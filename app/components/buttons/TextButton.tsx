8662bf748a719374377d7481061eb9a766e3b388
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-17 18:28:10 +0300

    Add dark mode to the button

--- app/components/buttons/TextButton.tsx
+++ app/components/buttons/TextButton.tsx
@@ -5,24 +5,36 @@ import { COLORS } from '@/constants/colors';
 import styled from 'styled-components';
 
 interface TextButtonProps {
-  label: string;
+  label?: string;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
   icon?: IconType;
-  width?: string;
+  darkMode?: boolean;
 }
 
-const ButtonStyle = styled.button<{ width: string }>`
+const ButtonStyle = styled.button<{ dark: boolean }>`
   display: flex;
   align-items: center;
   justify-content: baseline;
   gap: 4px;
   cursor: pointer;
-  color: ${COLORS.gray};
+  color: ${(props) => (props.dark ? COLORS.lightGray : COLORS.gray)};
   transition: color 0.2s transform 0.2s;
   &:hover {
-    color: ${COLORS.darkGray};
+    color: ${(props) => (props.dark ? '#ffffff' : COLORS.darkGray)};
   }
+`;
+
+const IconStyle = styled.div<{ dark: boolean }>`
+  font-size: 1rem;
+  padding: 3px 2px 4px 6px;
+  outline: 1px solid;
+  outline-color: ${(props) =>
+    props.dark ? COLORS.darkGray : COLORS.lightGray};
+  border-radius: 0.5rem;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
+  background-color: ${(props) =>
+    props.dark ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0.1)'};
 `;
 
 const TextButton = ({
@@ -30,12 +42,16 @@ const TextButton = ({
   onClick,
   disabled,
   icon: Icon,
-  width = 'auto',
+  darkMode = false,
 }: TextButtonProps) => {
   return (
-    <ButtonStyle disabled={disabled} width={width} onClick={onClick}>
+    <ButtonStyle dark={darkMode} disabled={disabled} onClick={onClick}>
       {label}
-      {Icon && <Icon size={14} />}
+      {Icon && (
+        <IconStyle dark={darkMode}>
+          <Icon />
+        </IconStyle>
+      )}
     </ButtonStyle>
   );
 };
