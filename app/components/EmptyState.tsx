2eebd600790ddca668f2f7ea9d886b6c2c26c7a9
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-18 21:30:59 +0300

    Update EmptyState.tsx

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,25 +1,13 @@
 'use client';
-
-import { useRouter } from 'next/navigation';
-import Button from './buttons/Button';
-
 interface EmptyStateProps {
   title?: string;
-  subtitle?: string;
-  showReset?: boolean;
 }
 
-const EmptyState: React.FC<EmptyStateProps> = ({
-  title = 'No exact matches',
-  subtitle = 'Try changing or removing some of your filters.',
-  showReset,
-}) => {
-  const router = useRouter();
-
+const EmptyState = ({ title = 'No exact matches' }: EmptyStateProps) => {
   return (
     <div
       className='
-        h-[60vh]
+        h-[80vh]
         flex 
         flex-col 
         gap-2 
@@ -29,15 +17,6 @@ const EmptyState: React.FC<EmptyStateProps> = ({
     >
       <div className='text-center'>
         <div className='text-2xl font-bold'>{title}</div>
-        <div className='font-light text-lg text-neutral-500 mt-2'>
-          {subtitle}
-        </div>
-      </div>
-
-      <div className='w-48 mt-4'>
-        {showReset && (
-          <Button label='Remove all filters' onClick={() => router.push('/')} />
-        )}
       </div>
     </div>
   );
