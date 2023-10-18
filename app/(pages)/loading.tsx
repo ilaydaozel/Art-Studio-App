7d28dd39cc3fbfb7e7b05039a931eff6f3b1a090
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-19 00:29:19 +0300

    Change loading's colors

--- app/(pages)/loading.tsx
+++ app/(pages)/loading.tsx
@@ -3,13 +3,10 @@ import React from 'react';
 import Spinner from '../components/utils/Spinner';
 export default function Loading() {
   return (
-    <div
-      style={{ backgroundColor: COLORS.darkGray }}
-      className='min-h-screen max-w-screen flex items-center justify-center z-20'
-    >
+    <div className='min-h-screen max-w-screen flex items-center justify-center z-20 bg-neutral-900'>
       <div className='text-center'>
         <Spinner />
-        <p className='text-3xl font-semibold mt-4 text-slate-300'>
+        <p className='text-3xl font-semibold mt-4 text-neutral-300'>
           Konak Kültür Sanat Akademisi
         </p>
       </div>
