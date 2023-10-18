e1b643042977d7c0b69606b7eb6d91a0efa5cd23
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-19 00:09:17 +0300

    Refactor edit exhibition page

--- app/(pages)/edit/(admin)/exhibition/[exhibitionId]/page.tsx
+++ app/(pages)/edit/(admin)/exhibition/[exhibitionId]/page.tsx
@@ -1,47 +1,47 @@
 import ClientOnly from '@/app/components/ClientOnly';
-import EmptyState from '@/app/components/EmptyState';
 import { IArtistProfile, IArtwork, IExhibition } from '@/app/types';
 import getExhibitionById from '@/app/actions/exhibition/getExhibitionById';
 import ExhibitionProfile from '@/app/components/exhibition/ExhibitionProfile';
 import getAllArtworks from '@/app/actions/artwork/getAllArtworks';
 import getAllArtistProfiles from '@/app/actions/artistProfile/getAllArtistProfiles';
+import EmptyState from '@/app/components/EmptyState';
 
 interface IParams {
   exhibitionId?: string;
 }
 
 const ExhibitionPage = async ({ params }: { params: IParams }) => {
-  let exhibition: { exhibition: IExhibition } | null = null;
-  let allArtworks: { artworks: IArtwork[] } | null = null;
-  let allArtistProfiles: { artistProfiles: IArtistProfile[] } | null = null;
-
-  if (params != undefined && params != undefined) {
+  let exhibition: { exhibition: IExhibition } | null;
+  let allArtworks: { artworks: IArtwork[] };
+  let allArtistProfiles: { artistProfiles: IArtistProfile[] };
+  try {
     exhibition = await getExhibitionById(params);
     allArtworks = await getAllArtworks();
     allArtistProfiles = await getAllArtistProfiles();
-  }
 
-  if (!exhibition) {
-    return (
-      <ClientOnly>
-        <EmptyState
-          title='An error occured.'
-          subtitle='Looks like this exhibiton does not exist anymore.'
-        />
-      </ClientOnly>
-    );
+    if (exhibition?.exhibition) {
+      return (
+        <ClientOnly>
+          <ExhibitionProfile
+            isEditable={true}
+            artworks={allArtworks.artworks}
+            exhibition={exhibition.exhibition}
+            allArtistProfiles={allArtistProfiles.artistProfiles}
+          ></ExhibitionProfile>
+        </ClientOnly>
+      );
+    } else {
+      return (
+        <ClientOnly>
+          <div className='bg-neutral-900'>
+            <EmptyState item='exhibition' />
+          </div>
+        </ClientOnly>
+      );
+    }
+  } catch (error: any) {
+    throw new Error(error);
   }
-
-  return (
-    <ClientOnly>
-      <ExhibitionProfile
-        isEditable={true}
-        artworks={allArtworks?.artworks}
-        exhibition={exhibition.exhibition}
-        allArtistProfiles={allArtistProfiles?.artistProfiles}
-      ></ExhibitionProfile>
-    </ClientOnly>
-  );
 };
 
 export default ExhibitionPage;
