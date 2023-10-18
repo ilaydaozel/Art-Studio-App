eed0f5ce263cf0638652dcfd8dde9a4583165a7d
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-19 00:04:24 +0300

    Refactor exhibitions page

--- app/(pages)/exhibitions/page.tsx
+++ app/(pages)/exhibitions/page.tsx
@@ -1,52 +1,24 @@
-import EmptyState from '@/app/components/EmptyState';
 import ClientOnly from '@/app/components/ClientOnly';
-import { IExhibition, IUser } from '@/app/types';
+import { IExhibition } from '@/app/types';
 import getAllExhibitions from '@/app/actions/exhibition/getAllExhibitions';
 import ExhibitionsClient from './ExhibitionsClient';
 
 const ExhibitionsPage = async () => {
-  let exhibitons: IExhibition[] | null = null;
+  let exhibitons: IExhibition[];
   try {
-    const result = await getAllExhibitions();
-    if (result && result.exhibitons) {
-      exhibitons = result.exhibitons;
-      if (exhibitons.length > 0) {
-        return (
-          <ClientOnly>
-            <div className='pt-28 w-full'>
-              <ExhibitionsClient exhibitions={exhibitons}></ExhibitionsClient>
-            </div>
-          </ClientOnly>
-        );
-      } else {
-        return (
-          <ClientOnly>
-            <EmptyState
-              title='No exhibitons found'
-              subtitle='Looks like you have no exhibitons.'
-            />
-          </ClientOnly>
-        );
-      }
-    } else {
+    const response = await getAllExhibitions();
+    if (response.exhibitons) {
+      exhibitons = response.exhibitons;
       return (
         <ClientOnly>
-          <EmptyState
-            title='An error occured.'
-            subtitle='Looks like you have no exhibitons.'
-          />
+          <div className='pt-28 w-full'>
+            <ExhibitionsClient exhibitions={exhibitons}></ExhibitionsClient>
+          </div>
         </ClientOnly>
       );
     }
-  } catch (error) {
-    return (
-      <ClientOnly>
-        <EmptyState
-          title='An error occured.'
-          subtitle='Looks like you have no exhibitons.'
-        />
-      </ClientOnly>
-    );
+  } catch (error: any) {
+    throw new Error(error);
   }
 };
 
