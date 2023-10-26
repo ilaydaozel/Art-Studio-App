91957b028c479e0dca15a7d7010d848dd2399dea
ilaydaozel <ilaydaoozel@gmail.com>
2023-10-26 23:52:05 +0300

    Update ContactClient

--- app/(pages)/contact/ContactClient.tsx
+++ app/(pages)/contact/ContactClient.tsx
@@ -1,12 +1,21 @@
 'use client';
 
 import GoogleMapsWidget from '@/app/components/utils/GoogleMapsWidget';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem;
+`;
 
 const ContactClient = () => {
   return (
-    <>
+    <Container>
       <GoogleMapsWidget />
-    </>
+    </Container>
   );
 };
 export default ContactClient;
